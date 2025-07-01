f4c66562c2af1efbe82d1196c328cb518f786d4f
Mohamed Sayhi <mohamedsayhii@outlook.com>
2025-07-02 00:59:50 +0100

    Add clock application to installs

--- install/xtras.sh
+++ install/xtras.sh
@@ -1,7 +1,7 @@
 yay -S --noconfirm --needed \
   signal-desktop spotify dropbox-cli zoom \
   obsidian typora libreoffice obs-studio kdenlive \
-  pinta xournalpp
+  pinta xournalpp gnome-clocks
 
 # Copy over Omarchy applications
 source ~/.local/share/omarchy/bin/omarchy-sync-applications
