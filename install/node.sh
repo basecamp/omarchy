fb9657954c2579b804ef5931836aa0fee71d45ed
Adam <adam.urban@gmail.com>
2025-07-07 09:18:22 +0200

    chore: updates

--- install/node.sh
+++ install/node.sh
@@ -1,6 +1,9 @@
 # #######################################################
 # Node with nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
+source ~/.bashrc
+
 nvm install 22
 corepack enable pnpm
 
