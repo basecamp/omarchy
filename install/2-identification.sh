0a03a0da3212e2bd4d8f66eedd16e36dad39c799
Adam <adam.urban@gmail.com>
2025-07-07 08:32:10 +0200

    chore: updates

--- install/2-identification.sh
+++ install/2-identification.sh
@@ -10,7 +10,7 @@ gpgkey=https://repo.charm.sh/yum/gpg.key' | sudo tee /etc/yum.repos.d/charm.repo
   sudo rpm --import https://repo.charm.sh/yum/gpg.key
 
   # yum
-  sudo yum install gum
+  sudo yum install -y gum
 fi
 
 # Configure identification
