bf47bb14a1d1d111ef27e0635ef87e6597b1ffd7
Adam <adam.urban@gmail.com>
2025-07-07 08:35:52 +0200

    chore: updates

--- install/3-terminal.sh
+++ install/3-terminal.sh
@@ -2,5 +2,5 @@ sudo dnf install -y \
   wget curl unzip net-tools \
   fd-find eza fzf ripgrep zoxide bat \
   wl-clipboard fastfetch btop \
-  man tldr less whois mlocate \
+  man tldr less whois \
   alacritty
