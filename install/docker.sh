047de29c57e7ce1a5d385edfac5906dba416d166
Adam <adam.urban@gmail.com>
2025-07-07 09:02:01 +0200

    chore: updates

--- install/docker.sh
+++ install/docker.sh
@@ -1,4 +1,7 @@
-sudo dnf install -y docker docker-compose
+sudo dnf -y install dnf-plugins-core
+sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Limit log size to avoid running out of disk
 sudo mkdir -p /etc/docker
