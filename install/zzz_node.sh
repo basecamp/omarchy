03a5cc14cd8efec4a347e2ea3ce1d06215b25912
Adam <adam.urban@gmail.com>
2025-07-07 09:40:04 +0200

    chore: updates

--- install/zzz_node.sh
+++ install/zzz_node.sh
@@ -2,7 +2,11 @@
 # Node with nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
-source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# source ~/.bashrc
 
 nvm install 22
 corepack enable pnpm
