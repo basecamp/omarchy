cff86335216beb3fe61ab03001977287cb190eb6
Ryan Hughes <1630358+ryanrhughes@users.noreply.github.com>
2025-07-19 19:47:48 -0400

    Add libqalculate so calc works out of the box

--- migrations/1752803055.sh
+++ migrations/1752803055.sh
@@ -1,4 +1,7 @@
 echo "Replace wofi with walker as the default launcher"
+yay -S --noconfirm --needed walker-bin libqalculate
+yay -Rns --noconfirm wofi
+
 if gum confirm "Launcher changed to walker. Would you like to remove the wofi config files?"; then
   rm -rf ~/.config/wofi
 fi
