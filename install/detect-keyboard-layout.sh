017d97a9c147c6ec144aacef2d51bf05695088c4
Saull Brandão <saullrb@proton.me>
2025-07-22 16:06:21 -0300

    align the identation

--- install/detect-keyboard-layout.sh
+++ install/detect-keyboard-layout.sh
@@ -7,9 +7,9 @@ layout=$(grep '^XKBLAYOUT=' "$conf" | cut -d= -f2 | tr -d '"')
 variant=$(grep '^XKBVARIANT=' "$conf" | cut -d= -f2 | tr -d '"')
 
 if [[ -n "$layout" ]]; then
-  sed -i "/^[[:space:]]*kb_options *=/i\    kb_layout = $layout" "$hyprconf"
+  sed -i "/^[[:space:]]*kb_options *=/i\  kb_layout = $layout" "$hyprconf"
 fi
 
 if [[ -n "$variant" ]]; then
-  sed -i "/^[[:space:]]*kb_options *=/i\    kb_variant = $variant" "$hyprconf"
+  sed -i "/^[[:space:]]*kb_options *=/i\  kb_variant = $variant" "$hyprconf"
 fi
