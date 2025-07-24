17fe9e1fdc1508d9eddcf7bd749a9212ad7d7b5c
Ryan Yogan <ryanyogan@gmail.com>
2025-07-24 13:27:43 -0500

    Optimizes install for arch linux of go package

--- install/monitor-settings.sh
+++ install/monitor-settings.sh
@@ -10,9 +10,12 @@ if [ -z "$OMARCHY_BARE" ] && ! command -v omarchy-monitor-settings &>/dev/null; 
   # Clone and build the monitor settings application
   git clone https://github.com/ryanyogan/omarchy-monitor-settings.git /tmp/omarchy-monitor-settings
   cd /tmp/omarchy-monitor-settings
-  go build -o omarchy-monitor-settings
+  export CGO_ENABLED=0
+  export GOOS=linux
+  go build -ldflags "-s -w" -o omarchy-monitor-settings
   sudo mv omarchy-monitor-settings /usr/local/bin/
   sudo chmod +x /usr/local/bin/omarchy-monitor-settings
   cd -
   rm -rf /tmp/omarchy-monitor-settings
-fi 
+fi
+
--- migrations/1753376254.sh
+++ migrations/1753376254.sh
@@ -9,9 +9,13 @@ if ! command -v omarchy-monitor-settings &>/dev/null; then
   # Clone and build the monitor settings application
   git clone https://github.com/ryanyogan/omarchy-monitor-settings.git /tmp/omarchy-monitor-settings
   cd /tmp/omarchy-monitor-settings
-  go build -o omarchy-monitor-settings
+
+  export CGO_ENABLED=0
+  export GOOS=linux
+  go build -ldflags "-s -w" -o omarchy-monitor-settings
+
   sudo mv omarchy-monitor-settings /usr/local/bin/
   sudo chmod +x /usr/local/bin/omarchy-monitor-settings
   cd -
   rm -rf /tmp/omarchy-monitor-settings
-fi 
+fi
