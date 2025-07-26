93b0090ddf92732a2b614e451fdc893846e8d8dd
Kyle Kwong <kylekwong1@gmail.com>
2025-07-26 15:04:18 -0700

    Small edit for temp directory best practice

--- install/preflight/aur.sh
+++ install/preflight/aur.sh
@@ -25,15 +25,14 @@ fi
 if ! command -v yay &>/dev/null; then
   # Install build tools
   sudo pacman -Sy --needed --noconfirm base-devel
-  cd /tmp
-  rm -rf yay-bin
-  git clone https://aur.archlinux.org/yay-bin.git
-  cd yay-bin
+  tempdir=$(mktemp -d)  
+  trap 'rm -rf "$tempdir"' EXIT
+  cd "$tempdir" && \
+  git clone https://aur.archlinux.org/yay-bin.git && \
+  cd yay-bin && \
   makepkg -si --noconfirm
-  cd -
-  rm -rf yay-bin
-  cd ~
-fi
+  
+  fi
 
 # Add fun and color to the pacman installer
 if ! grep -q "ILoveCandy" /etc/pacman.conf; then
