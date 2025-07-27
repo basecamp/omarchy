e16be0bd5e7c5da797b19183d1280977a181ce2d
Ryan Yogan <ryanyogan@gmail.com>
2025-07-26 22:10:07 -0500

    Updates install method

--- install/monitor-settings.sh
+++ install/monitor-settings.sh
@@ -7,25 +7,9 @@ if [ -z "$OMARCHY_BARE" ] && ! command -v omarchy-monitor-settings &>/dev/null; 
     mise use -g go@latest
   fi
 
-  # Clone and build the monitor settings application
-
   # TODO: Discuss where this should live
-  git clone https://github.com/ryanyogan/omarchy-monitor-settings.git /tmp/omarchy-monitor-settings
-  cd /tmp/omarchy-monitor-settings
+  go install github.com/ryanyogan/omarchy-monitor-settings@latest
 
-  git checkout v1.1.1
-
-  export CGO_ENABLED=0
-  export GOOS=linux
-
-  VERSION=$(git describe --tags --always --dirty 2>/dev/null || echo "dev")
-  go build -ldflags "-s -w -X main.version=${VERSION}" -o omarchy-monitor-settings .
-  git clone https://github.com/ryanyogan/omarchy-monitor-settings.git /tmp/omarchy-monitor-settings
-
-  cd /tmp/omarchy-monitor-settings
   sudo mv omarchy-monitor-settings /usr/local/bin/
   sudo chmod +x /usr/local/bin/omarchy-monitor-settings
-
-  cd -
-  rm -rf /tmp/omarchy-monitor-settings
 fi
--- migrations/1753376254.sh
+++ migrations/1753376254.sh
@@ -6,21 +6,9 @@ if ! command -v omarchy-monitor-settings &>/dev/null; then
     mise use -g go@latest
   fi
 
-  # Clone and build the monitor settings application
   # TODO: Discuss where this should live
-  git clone https://github.com/ryanyogan/omarchy-monitor-settings.git /tmp/omarchy-monitor-settings
-  cd /tmp/omarchy-monitor-settings
-
-  git checkout v1.1.1
-
-  export CGO_ENABLED=0
-  export GOOS=linux
-
-  VERSION=$(git describe --tags --always --dirty 2>/dev/null || echo "dev")
-  go build -ldflags "-s -w -X main.version=${VERSION}" -o omarchy-monitor-settings .
+  go install github.com/ryanyogan/omarchy-monitor-settings@latest
 
   sudo mv omarchy-monitor-settings /usr/local/bin/
   sudo chmod +x /usr/local/bin/omarchy-monitor-settings
-  cd -
-  rm -rf /tmp/omarchy-monitor-settings
 fi
