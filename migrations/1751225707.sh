16347910d6268bd69de63c82e645b672300b3c83
Ryan Hughes <1630358+ryanrhughes@users.noreply.github.com>
2025-08-02 01:58:32 -0400

    Replace -SIGUSR2 waybar instances

--- migrations/1751225707.sh
+++ migrations/1751225707.sh
@@ -2,5 +2,5 @@ echo "Fixing persistent workspaces in waybar config"
 
 if [[ -f ~/.config/waybar/config ]]; then
   sed -i 's/"persistent_workspaces":/"persistent-workspaces":/' ~/.config/waybar/config
-  pkill -SIGUSR2 waybar
+  omarchy-cmd-restart-waybar
 fi
