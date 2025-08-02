49ebf13be6ff342176469745c0da5c2c0c8c56d2
Jaxon Wright <jaxonhwright@gmail.com>
2025-08-02 17:36:03 -0400

    update hyprsunset migration to skip yay install if already installed

--- migrations/1754109724.sh
+++ migrations/1754109724.sh
@@ -1,5 +1,7 @@
 echo "Add hyprsunset blue light filter"
-yay -S --noconfirm --needed hyprsunset
+if ! command -v hyprsunset &>/dev/null; then
+    yay -S --noconfirm --needed hyprsunset
+fi
 cp -r ~/.local/share/omarchy/config/hypr/hyprsunset.conf ~/.config/hypr/
 pkill hyprsunset
 setsid uwsm app -- hyprsunset &>/dev/null &
