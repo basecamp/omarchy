45578638132fc4b7ff04a6fdda0f16c60135a43e
Taha <paprikataha@gmail.com>
2025-08-06 01:16:02 -0700

    remove lazyvim config and add omarchy nvim folder

---
+++ config/nvim/omarchy/init.lua
@@ -0,0 +1,27 @@
+return {
+  plugins = {
+    -- Themes
+    { "ellisonleao/gruvbox.nvim", lazy = false, priority = 1000 },
+    { "catppuccin/nvim", name = "catppuccin", lazy = false, priority = 1000 },
+    { "folke/tokyonight.nvim", lazy = false, priority = 1000 },
+    { "neanias/everforest-nvim", lazy = false, priority = 1000 },
+    { "rebelot/kanagawa.nvim", lazy = false, priority = 1000 },
+    { "tahayvr/matteblack.nvim", lazy = false, priority = 1000 },
+    { "EdenEast/nightfox.nvim", lazy = false, priority = 1000 },
+    { "rose-pine/neovim", name = "rose-pine", lazy = false, priority = 1000 },
+  },
+  install = {
+    colorscheme = {
+      "catppuccin-latte",
+      "catppuccin-mocha",
+      "tokyonight",
+      "habamax",
+      "gruvbox",
+      "everforest",
+      "kanagawa",
+      "nordfox",
+      "matteblack",
+      "rose-pine-dawn",
+    },
+  },
+}
