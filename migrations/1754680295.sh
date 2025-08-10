4bd7b31d69221702edbad9aa3114044cc6c11e4e
Ryan Hughes <ryan@heyoodle.com>
2025-08-10 14:52:37 +0200

    Refresh after update

--- migrations/1754680295.sh
+++ migrations/1754680295.sh
@@ -1,3 +1,5 @@
 echo "Add UWSM env"
 mkdir -p "$HOME/.config/uwsm/"
 omarchy-refresh-config uwsm/env
+
+gum confirm "UWSM env has been updated and a relaunch of Hyprland is required to apply the changes. Would you like to relaunch Hyprland now?" && uwsm stop
