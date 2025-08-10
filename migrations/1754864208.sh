a82531e7cfb5af5d9a5718fa5f0edd6dd04ebddb
Martin Mose Hansen <martinmosehansen@me.com>
2025-08-11 00:35:55 +0200

    feat: add custom logo to fastfetch

---
+++ migrations/1754864208.sh
@@ -0,0 +1,4 @@
+echo "Update fastfetch with custom Omarchy logo"
+
+~/.local/share/omarchy/bin/omarchy-refresh-config fastfetch/config.jsonc
+~/.local/share/omarchy/bin/omarchy-refresh-config fastfetch/omarchy-logo.txt
