d527d0f4113d48cbe004408e8119eba83f7e5d66
Tomás Machado <59934625+ttmachado@users.noreply.github.com>
2025-08-10 23:28:06 +0100

    chore: clear terminal before starting the installation

--- install.sh
+++ install.sh
@@ -26,6 +26,7 @@ show_subtext() {
 }
 
 # Install prerequisites
+clear
 echo -e "Installing prerequisites [0/5]"
 source $OMARCHY_INSTALL/preflight/aur.sh
 source $OMARCHY_INSTALL/preflight/presentation.sh
