c6a17728209db30d4262a57c55467ded7bb183ff
Ryan Hughes <ryan@heyoodle.com>
2025-08-10 16:37:17 +0200

    Set state instead

--- migrations/1754680295.sh
+++ migrations/1754680295.sh
@@ -2,4 +2,4 @@ echo "Add UWSM env"
 mkdir -p "$HOME/.config/uwsm/"
 omarchy-refresh-config uwsm/env
 
-gum confirm "UWSM env has been updated and a relaunch of Hyprland is required to apply the changes. Would you like to relaunch Hyprland now?" && uwsm stop
+omarchy-state set relaunch-required
