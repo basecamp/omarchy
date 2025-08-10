4de85cfcc471d9c0b10537addc036c328082f2b2
Ryan Hughes <ryan@heyoodle.com>
2025-08-10 23:38:52 +0200

    Fix migration

--- migrations/1754860578.sh
+++ migrations/1754860578.sh
@@ -2,7 +2,7 @@ echo "Update polkit policy to yield to fingerprint and fido2"
 # If fprint exists in polkit, it was wrong and needs reset
 if [ -f /etc/pam.d/polkit-1 ] && grep -Fq 'pam_fprintd.so' /etc/pam.d/polkit-1; then
   sudo tee /etc/pam.d/polkit-1 >/dev/null <<'EOF'
-auth      sufficient pam_u2f.so cue authfile=/etc/fido2/fido2
+auth      sufficient pam_fprintd.so
 auth      required pam_unix.so
 
 account   required pam_unix.so
