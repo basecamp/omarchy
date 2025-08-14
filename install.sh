829cb2f91f166239feee931e21b3c8a1caf81313
Julien Dambron <dreamlike3d@gmail.com>
2025-08-14 10:03:52 +0200

    Restore normal uwsm updates
    Now that uwsm v0.23.2 is available in the Extra repository, restore to
    normal

--- install.sh
+++ install.sh
@@ -77,7 +77,7 @@ source $OMARCHY_INSTALL/apps/mimetypes.sh
 show_logo highlight
 show_subtext "Updating system packages [5/5]"
 sudo updatedb
-yay -Syu --noconfirm --ignore uwsm
+yay -Syu --noconfirm
 
 # Reboot
 show_logo laseretch 920
