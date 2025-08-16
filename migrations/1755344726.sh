5299f383f84c22da6a7061a4dc154bb0be6efc45
jardahrazdera <jarek@jaroslav.tech>
2025-08-16 13:48:38 +0200

    Fix About menu not opening
    The About.desktop file exists but wasn't being deployed to users.
    Added migration to copy it to the applications directory where
    gtk-launch can find it.
    
    Users were clicking About in the menu but nothing happened because
    the desktop file wasn't in the expected location.
    
    Fixes #845

---
+++ migrations/1755344726.sh
@@ -0,0 +1,4 @@
+echo "Install About.desktop for system information menu"
+
+cp $OMARCHY_PATH/applications/About.desktop ~/.local/share/applications/
+update-desktop-database ~/.local/share/applications/
