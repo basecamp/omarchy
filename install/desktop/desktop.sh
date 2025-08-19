146620435d9f92f2ea82b8ed36e5744557e93784
johnny <enriquemartin1402@gmail.com>
2025-08-19 11:22:39 +0200

    Install yazi

--- install/desktop/desktop.sh
+++ install/desktop/desktop.sh
@@ -4,6 +4,7 @@ yay -S --noconfirm --needed \
   brightnessctl playerctl pamixer wiremix wireplumber \
   fcitx5 fcitx5-gtk fcitx5-qt wl-clip-persist \
   nautilus sushi ffmpegthumbnailer gvfs-mtp \
+  yazi \
   slurp satty \
   mpv evince imv \
   chromium
@@ -14,3 +15,5 @@ if lspci | grep -qi 'nvidia'; then
 else
   yay -S --noconfirm --needed wl-screenrec
 fi
+
+omarchy-install-yazi
