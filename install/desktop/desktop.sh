af53258606173599b0c7e4a016b3e243c5bb7113
johnny <enriquemartin1402@gmail.com>
2025-08-20 12:06:06 +0200

    Add ueberzugpp for image preview support in Alacritty

--- install/desktop/desktop.sh
+++ install/desktop/desktop.sh
@@ -4,7 +4,7 @@ yay -S --noconfirm --needed \
   brightnessctl playerctl pamixer wiremix wireplumber \
   fcitx5 fcitx5-gtk fcitx5-qt wl-clip-persist \
   nautilus sushi ffmpegthumbnailer gvfs-mtp \
-  yazi \
+  yazi ueberzugpp \
   slurp satty \
   mpv evince imv \
   chromium
