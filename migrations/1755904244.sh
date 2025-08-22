1e675c26e13c568cfd3c019f936c7057c9f14053
Taha <paprikataha@gmail.com>
2025-08-22 16:16:50 -0700

    Add migration file for fastfetch update

---
+++ migrations/1755904244.sh
@@ -0,0 +1,7 @@
+echo "Update fastfetch config with new Omarchy logo"
+
+~/.local/share/omarchy/bin/omarchy-refresh-config fastfetch/config.jsonc
+~/.local/share/omarchy/bin/omarchy-refresh-config fastfetch/omarchy-icon.txt
+
+
+
