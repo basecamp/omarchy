a0a8dfabd3beea5b7319a5ccb0f7316339201bfd
David Heinemeier Hansson <david@hey.com>
2025-08-22 17:26:33 +0200

    setsid so it doesn't exit with the terminal

--- migrations/1755208929.sh
+++ migrations/1755208929.sh
@@ -1,5 +1,5 @@
 echo "Change behaviour of XF86PowerOff button to show omarchy system menu insead of shutting down immediately"
 
 source $OMARCHY_PATH/install/config/ignore-power-button.sh
-# Whole hyprland got killed when I try to use `systemctl restart systemd-logind` so here is a workaround to handle it 
-systemd-inhibit --what=handle-power-key --why="Temporary disable power button before restart" sleep infinity &
+setsid systemd-inhibit --what=handle-power-key --why="Temporary disable power button before restart" sleep infinity &
+
