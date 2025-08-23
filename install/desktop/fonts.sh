1e77c165266fd08c91760ca39c39829d06d4a93b
David Heinemeier Hansson <david@hey.com>
2025-08-23 23:22:11 +0200

    Fixup installation of the omarchy font

--- install/desktop/fonts.sh
+++ install/desktop/fonts.sh
@@ -2,7 +2,9 @@
 
 yay -S --noconfirm --needed ttf-font-awesome ttf-cascadia-mono-nerd ttf-ia-writer noto-fonts noto-fonts-emoji
 
-cp ~/.local/share/omarchy/config/omarchy.ttf /usr/share/fonts/
+mkdir -p ~/.local/share/fonts
+cp ~/.local/share/omarchy/config/omarchy.ttf ~/.local/share/fonts/
+fc-cache
 
 if [ -z "$OMARCHY_BARE" ]; then
   yay -S --noconfirm --needed ttf-jetbrains-mono noto-fonts-cjk noto-fonts-extra
--- migrations/1755981930.sh
+++ migrations/1755981930.sh
@@ -1,6 +1,8 @@
 echo "Add Omarchy icon to the Waybar"
 
-cp ~/.local/share/omarchy/config/omarchy.ttf /usr/share/fonts/
+mkdir -p ~/.local/share/fonts
+cp ~/.local/share/omarchy/config/omarchy.ttf ~/.local/share/fonts/
+fc-cache
 
 echo
 gum confirm "Replace current Waybar config (backup will be made)?" && omarchy-refresh-waybar
