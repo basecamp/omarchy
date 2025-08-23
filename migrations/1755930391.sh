e0d9baff858f6e6b7172021eb199f7151fa8b637
Andrej Benz <hello@benz.dev>
2025-08-23 08:34:59 +0200

    migration: walker 1.0.0-beta

---
+++ migrations/1755930391.sh
@@ -0,0 +1,28 @@
+echo "Migrate to Walker 1.0.0-Beta"
+
+pkill walker
+
+yay -Rns --noconfirm walker-bin
+
+yay -S --noconfirm --needed \
+  elephant \
+  elephant-calc \
+  elephant-clipboard \
+  elephant-desktopapplications \
+  elephant-files \
+  elephant-menus \
+  elephant-providerlist \
+  elephant-runner \
+  elephant-symbols \
+  elephant-websearch \
+  walker
+
+rm -rf ~/.config/walker
+mkdir -p ~/.config/walker
+mkdir -p ~/.config/elephant
+
+cp -r ~/.local/share/omarchy/config/walker/* ~/.config/walker/
+cp -r ~/.local/share/omarchy/config/elephant/* ~/.config/elephant/
+
+omarchy-refresh-walker
+omarchy-refresh-elephant
