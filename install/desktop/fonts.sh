07505a82903ef15df0800ae24c24c625570c4700
Taha <paprikataha@gmail.com>
2025-08-23 14:00:01 -0700

    Add step to fonts.sh to install omarchy icon font

--- install/desktop/fonts.sh
+++ install/desktop/fonts.sh
@@ -2,6 +2,8 @@
 
 yay -S --noconfirm --needed ttf-font-awesome ttf-cascadia-mono-nerd ttf-ia-writer noto-fonts noto-fonts-emoji
 
+cp "$HOME/.local/share/omarchy/config/omarchy.ttf" /usr/share/fonts
+
 if [ -z "$OMARCHY_BARE" ]; then
   yay -S --noconfirm --needed ttf-jetbrains-mono noto-fonts-cjk noto-fonts-extra
 fi
