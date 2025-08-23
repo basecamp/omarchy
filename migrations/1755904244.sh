52fcd759cb1bdfe440339aea3b08840571553a3e
David Heinemeier Hansson <david@hey.com>
2025-08-23 07:35:03 +0200

    Use consistent branding placement for spots you can change

--- migrations/1755904244.sh
+++ migrations/1755904244.sh
@@ -1,7 +1,6 @@
 echo "Update fastfetch config with new Omarchy logo"
 
-~/.local/share/omarchy/bin/omarchy-refresh-config fastfetch/config.jsonc
-~/.local/share/omarchy/bin/omarchy-refresh-config fastfetch/omarchy-icon.txt
+omarchy-refresh-config fastfetch/config.jsonc
 
-
-
+mkdir -p ~/.config/omarchy/branding
+cp $OMARCHY_PATH/icon.txt ~/.config/omarchy/branding/about.txt
