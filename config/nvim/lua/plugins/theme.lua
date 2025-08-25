6b0b21230fdb7dc5d9aa9ff601c00d172788aaae
ramtinJ <ramme95@gmail.com>
2025-08-25 21:12:49 +0200

    Quick fix for lazyvim bufferline error when using catppuccin

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -5,4 +5,25 @@ return {
 			colorscheme = "tokyonight",
 		},
 	},
+	{
+		"akinsho/bufferline.nvim",
+		-- TODO: Remove this once its fixed in lazyvim. This is only a temporary fix, and its onl needed for catppuccin
+		init = function()
+			local bufline = require("catppuccin.groups.integrations.bufferline")
+			bufline.get = bufline.get_theme
+		end,
+		---@module 'bufferline'
+		---@type bufferline.Config
+		opts = {
+			options = {
+				always_show_bufferline = true,
+				separator_style = "thick",
+				hover = {
+					enabled = true,
+					delay = 120,
+					reveal = { "close" },
+				},
+			},
+		},
+	},
 }
