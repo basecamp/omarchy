42698c70ec65f8a3b9a2b4ebd67590a030d0c253
ramtinJ <ramme95@gmail.com>
2025-08-25 21:37:47 +0200

    Adding the fix in theme, actually messed up the theme colors

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -5,16 +5,4 @@ return {
 			colorscheme = "tokyonight",
 		},
 	},
-	{
-		{
-			"akinsho/bufferline.nvim",
-			-- TODO: Remove this once its fixed in lazyvim. This is only a temporary fix, and its onl needed for catppuccin
-			init = function()
-				local bufline = require("catppuccin.groups.integrations.bufferline")
-				function bufline.get()
-					return bufline.get_theme()
-				end
-			end,
-		},
-	},
 }
