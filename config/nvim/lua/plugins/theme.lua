51d797f0fb66c13ec98ba587249208ed000d5728
ramtinJ <ramme95@gmail.com>
2025-08-25 21:32:16 +0200

    This is a slightly better fix

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -6,24 +6,15 @@ return {
 		},
 	},
 	{
-		"akinsho/bufferline.nvim",
-		-- TODO: Remove this once its fixed in lazyvim. This is only a temporary fix, and its onl needed for catppuccin
-		init = function()
-			local bufline = require("catppuccin.groups.integrations.bufferline")
-			bufline.get = bufline.get_theme
-		end,
-		---@module 'bufferline'
-		---@type bufferline.Config
-		opts = {
-			options = {
-				always_show_bufferline = true,
-				separator_style = "thick",
-				hover = {
-					enabled = true,
-					delay = 120,
-					reveal = { "close" },
-				},
-			},
+		{
+			"akinsho/bufferline.nvim",
+			-- TODO: Remove this once its fixed in lazyvim. This is only a temporary fix, and its onl needed for catppuccin
+			init = function()
+				local bufline = require("catppuccin.groups.integrations.bufferline")
+				function bufline.get()
+					return bufline.get_theme()
+				end
+			end,
 		},
 	},
 }
