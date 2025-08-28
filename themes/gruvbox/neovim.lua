4e1d69088946c95ce5425bfbf2d3b999e3520999
معتز <128713962+motaz-shokry@users.noreply.github.com>
2025-08-29 01:06:56 +0300

    Update neovim.lua
    A better gruvbox theme for neovim

--- themes/gruvbox/neovim.lua
+++ themes/gruvbox/neovim.lua
@@ -1,8 +1,16 @@
 return {
-	"https://gitlab.com/motaz-shokry/gruvbox.nvim",
-	name = "gruvbox",
-    priority = 1000,
-	config = function()
-		vim.cmd("colorscheme gruvbox")
-	end
+	{
+		"https://gitlab.com/motaz-shokry/gruvbox.nvim",
+		name = "gruvbox",
+    	priority = 1000,
+		config = function()
+			vim.cmd("colorscheme gruvbox")
+		end
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "gruvbox",
+		},
+	},
 }
