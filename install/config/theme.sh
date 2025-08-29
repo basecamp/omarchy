215067f31c6827b123314f3434254019ca14b1de
robert stringer <bitmap+github@gmail.com>
2025-08-29 21:35:43 +0100

    added gtk-update-icon-cache for symlinked files

--- install/config/theme.sh
+++ install/config/theme.sh
@@ -7,6 +7,7 @@ gsettings set org.gnome.desktop.interface icon-theme "Yaru-blue"
 # Set links for Nautilius action icons
 sudo ln -snf /usr/share/icons/Adwaita/symbolic/actions/go-previous-symbolic.svg /usr/share/icons/Yaru/scalable/actions/go-previous-symbolic.svg
 sudo ln -snf /usr/share/icons/Adwaita/symbolic/actions/go-next-symbolic.svg /usr/share/icons/Yaru/scalable/actions/go-next-symbolic.svg
+sudo gtk-update-icon-cache /usr/share/icons/Yaru
 
 # Setup theme links
 mkdir -p ~/.config/omarchy/themes
