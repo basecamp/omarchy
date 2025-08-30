2868f512777d1f6b2031137eb17a23043f1aa326
David Heinemeier Hansson <david@hey.com>
2025-08-30 07:37:04 +0200

    Update font installation script for JetBrains Mono

--- migrations/1756507384.sh
+++ migrations/1756507384.sh
@@ -1,2 +1,4 @@
-#!/bin/bash
-omarchy-pkg-install ttf-jetbrains-mono-nerd
+echo "Replace JetBrains Mono font with the Nerd Font edition"
+
+pkg-add ttf-jetbrains-mono-nerd
+pkg-remove ttf-jetbrains-mono
