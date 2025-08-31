2812b774d6322c1679c9763aee7843eae7767d32
David Heinemeier Hansson <david@hey.com>
2025-08-31 09:07:34 +0200

    Cleanup and ensure successful exit so as not to halt migrations

--- migrations/1756205202.sh
+++ migrations/1756205202.sh
@@ -2,9 +2,6 @@ echo "Symlink files needed for Nautilus navigation icons"
 
 sudo ln -snf /usr/share/icons/Adwaita/symbolic/actions/go-previous-symbolic.svg /usr/share/icons/Yaru/scalable/actions/go-previous-symbolic.svg
 sudo ln -snf /usr/share/icons/Adwaita/symbolic/actions/go-next-symbolic.svg /usr/share/icons/Yaru/scalable/actions/go-next-symbolic.svg
-
-# rebuild cache
 sudo gtk-update-icon-cache /usr/share/icons/Yaru
+pkill -x nautilus || true
 
-echo "Close Files if running"
-sudo -u ${SUDO_USER:-$USER} pkill nautilus
