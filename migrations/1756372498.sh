eb3a3774e75b6c9085fcb9815ccd0397e3b3b337
Stefan Gruendel <stefan.gruendel@gmail.com>
2025-08-31 12:11:31 +0200

    refactor migration script to only link eza theme if it exists for current theme

--- migrations/1756372498.sh
+++ migrations/1756372498.sh
@@ -1,4 +1,7 @@
 echo "Add eza config directory"
 
 mkdir -p ~/.config/eza
-ln -snf ~/.config/omarchy/current/theme/eza-theme.yml ~/.config/eza/theme.yml
+# not all themes have eza config, only link if it exists
+if [ -f ~/.config/omarchy/current/theme/eza-theme.yml ]; then
+  ln -snf ~/.config/omarchy/current/theme/eza-theme.yml ~/.config/eza/theme.yml
+fi
