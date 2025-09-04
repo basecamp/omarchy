f29a44fb796bc53d49c70bf905906bd20b6c5433
RATIU5 <jcmemmott20@outlook.com>
2025-09-04 14:18:23 -0600

    adjusted config to better suit my workflow

--- install.sh
+++ install.sh
@@ -19,7 +19,6 @@ source $OMARCHY_INSTALL/preflight/first-run-mode.sh
 # Packaging
 source $OMARCHY_INSTALL/packages.sh
 source $OMARCHY_INSTALL/packaging/fonts.sh
-source $OMARCHY_INSTALL/packaging/lazyvim.sh
 source $OMARCHY_INSTALL/packaging/webapps.sh
 source $OMARCHY_INSTALL/packaging/tuis.sh
 
@@ -35,7 +34,6 @@ source $OMARCHY_INSTALL/config/increase-lockout-limit.sh
 source $OMARCHY_INSTALL/config/ssh-flakiness.sh
 source $OMARCHY_INSTALL/config/detect-keyboard-layout.sh
 source $OMARCHY_INSTALL/config/xcompose.sh
-source $OMARCHY_INSTALL/config/mise-ruby.sh
 source $OMARCHY_INSTALL/config/docker.sh
 source $OMARCHY_INSTALL/config/mimetypes.sh
 source $OMARCHY_INSTALL/config/localdb.sh
