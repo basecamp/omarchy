acee5d619fe8853c4b4dbee98b1a72bff9279ff0
David Heinemeier Hansson <david@hey.com>
2025-09-10 15:24:25 +0200

    Don't need any of this that isn't seen or used

--- default/waybar/indicators/screen-recording.sh
+++ default/waybar/indicators/screen-recording.sh
@@ -3,5 +3,5 @@
 if pgrep -x wl-screenrec >/dev/null || pgrep -x wf-recorder >/dev/null; then
   echo '{"text": "󰻂", "tooltip": "Stop recording", "class": "active"}'
 else
-  echo '{"text": " ", "tooltip": "Not recording", "class": "inactive"}'
+  echo '{"text": " "}'
 fi
