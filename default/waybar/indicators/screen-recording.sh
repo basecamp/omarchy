59a24efc35fe5af9fdfbf238173400c5525b2700
David Heinemeier Hansson <david@hey.com>
2025-09-10 15:32:30 +0200

    Better just to move the clock rather than have it be permanently offset

--- default/waybar/indicators/screen-recording.sh
+++ default/waybar/indicators/screen-recording.sh
@@ -3,5 +3,5 @@
 if pgrep -x wl-screenrec >/dev/null || pgrep -x wf-recorder >/dev/null; then
   echo '{"text": "󰻂", "tooltip": "Stop recording", "class": "active"}'
 else
-  echo '{"text": " "}'
+  echo '{"text": ""}'
 fi
