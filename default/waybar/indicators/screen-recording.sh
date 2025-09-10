ced9c6c5801509d16cbe43d33bb0cdd6e2033f67
David Heinemeier Hansson <david@hey.com>
2025-09-10 15:00:42 +0200

    Sharpen icons and add on-click to stop

--- default/waybar/indicators/screen-recording.sh
+++ default/waybar/indicators/screen-recording.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if pgrep -x wl-screenrec >/dev/null || pgrep -x wf-recorder >/dev/null; then
-  echo '{"text": "⏺", "tooltip": "Recording in progress", "class": "recording"}'
+  echo '{"text": "󰻂", "tooltip": "Stop recording", "class": "recording", "on-click": "omarchy-cmd-screenrecord-stop"}'
 else
-  echo '{"text": "", "tooltip": "Not recording", "class": "idle"}'
+  echo '{"text": " ", "tooltip": "Not recording", "class": "idle"}'
 fi
