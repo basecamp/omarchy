480138a9d2aa8dd926a607f9bffb463cef59d383
David Heinemeier Hansson <david@hey.com>
2025-09-10 15:11:33 +0200

    Don't need to repeat the on-click

--- default/waybar/indicators/screen-recording.sh
+++ default/waybar/indicators/screen-recording.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if pgrep -x wl-screenrec >/dev/null || pgrep -x wf-recorder >/dev/null; then
-  echo '{"text": "󰻂", "tooltip": "Stop recording", "class": "recording", "on-click": "omarchy-cmd-screenrecord-stop"}'
+  echo '{"text": "󰻂", "tooltip": "Stop recording", "class": "recording"}'
 else
   echo '{"text": " ", "tooltip": "Not recording", "class": "idle"}'
 fi
