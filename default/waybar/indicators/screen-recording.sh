8bbf67c381e744548241395c33b977242f968729
David Heinemeier Hansson <david@hey.com>
2025-09-10 15:16:17 +0200

    Naming

--- default/waybar/indicators/screen-recording.sh
+++ default/waybar/indicators/screen-recording.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if pgrep -x wl-screenrec >/dev/null || pgrep -x wf-recorder >/dev/null; then
-  echo '{"text": "󰻂", "tooltip": "Stop recording", "class": "recording"}'
+  echo '{"text": "󰻂", "tooltip": "Stop recording", "class": "active"}'
 else
-  echo '{"text": " ", "tooltip": "Not recording", "class": "idle"}'
+  echo '{"text": " ", "tooltip": "Not recording", "class": "inactive"}'
 fi
