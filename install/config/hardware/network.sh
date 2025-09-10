2fc00bc9cddf1d09a705a000ffcf4c8df388cca9
Augusto Magno <49214136+aquaspy@users.noreply.github.com>
2025-09-09 23:14:18 -0300

    Add symlink to use systemd‑resolved stub DNS configuration

--- install/config/hardware/network.sh
+++ install/config/hardware/network.sh
@@ -6,3 +6,6 @@ sudo systemctl enable iwd.service
 # Prevent systemd-networkd-wait-online timeout on boot
 sudo systemctl disable systemd-networkd-wait-online.service
 sudo systemctl mask systemd-networkd-wait-online.service
+
+# Provide domain name resolution for software that reads /etc/resolv.conf directly
+sudo ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
