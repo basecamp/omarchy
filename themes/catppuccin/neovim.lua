e488d6e3da7a02dfae28028509523f284afad155
dotsilva <devbrunodasilva@gmail.com>
2025-09-11 03:06:28 -0300

    Update neovim.lua
    Workaround to fix error when starting neovim with any catppuccin default theme in Omarchy

--- themes/catppuccin/neovim.lua
+++ themes/catppuccin/neovim.lua
@@ -5,4 +5,14 @@ return {
 			colorscheme = "catppuccin",
 		},
 	},
+	
+	{
+    "akinsho/bufferline.nvim",
+    init = function()
+      local bufline = require("catppuccin.groups.integrations.bufferline")
+      function bufline.get()
+        return bufline.get_theme()
+      end
+    end,
+  },
 }
