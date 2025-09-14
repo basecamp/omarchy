4a27cfe651a813faba269b6579e94c465643216c
Adam Michaels <ilovedt@gmail.com>
2025-09-14 13:12:31 -0700

    DND: whitelist Omarchy toasts and critical alerts\n\n- Tag internal notify-send calls with -a omarchy so system toasts show during DND.\n- Allow critical urgency during DND via [mode=do-not-disturb urgency=critical].\n- Update Hyprland DND toggle and first-run helpers to use -a omarchy.

--- install/first-run/welcome.sh
+++ install/first-run/welcome.sh
@@ -1,1 +1,1 @@
-notify-send "👋   Welcome to Omarchy" "You're in for a great computing adventure. Have fun!" -t 30000
+notify-send -a omarchy "👋   Welcome to Omarchy" "You're in for a great computing adventure. Have fun!" -t 30000
