20c4d7d1f939c3e6d3332c23341037fcb17cca8f
Ryan Hughes <ryan@heyoodle.com>
2025-09-16 11:05:22 -0400

    Add walker restart

--- install/config/walker-elephant.sh
+++ install/config/walker-elephant.sh
@@ -5,3 +5,19 @@ install -D -m 644 $OMARCHY_PATH/default/systemd/user/elephant.service ~/.config/
 
 # Enable the service
 systemctl enable --user elephant.service
+
+# Create pacman hook to restart walker after updates
+sudo mkdir -p /etc/pacman.d/hooks
+sudo tee /etc/pacman.d/hooks/walker-restart.hook > /dev/null << EOF
+[Trigger]
+Type = Package
+Operation = Upgrade
+Target = walker
+Target = walker-debug
+Target = elephant*
+
+[Action]
+Description = Restarting Walker services after system update
+When = PostTransaction
+Exec = $OMARCHY_PATH/bin/omarchy-restart-walker
+EOF
--- migrations/1755930391.sh
+++ migrations/1755930391.sh
@@ -17,14 +17,7 @@ omarchy-pkg-add elephant \
   elephant-websearch \
   walker
 
-rm -rf ~/.config/walker
-mkdir -p ~/.config/walker
-mkdir -p ~/.config/elephant
-
-cp -r ~/.local/share/omarchy/config/walker/* ~/.config/walker/
-cp -r ~/.local/share/omarchy/config/elephant/* ~/.config/elephant/
-
-install -D -m 644 $OMARCHY_PATH/default/systemd/user/elephant.service ~/.config/systemd/user/elephant.service
+source $OMARCHY_PATH/install/config/walker-elephant.sh
 
 systemctl enable --now --user elephant.service
 omarchy-restart-walker
