b36716a7387e4e920b3117ff6d6d087853d9684f
Koen van Eijk <vaneijk.koen@gmail.com>
2025-09-18 10:23:32 +0200

    Added --needed flag to pacman calls to speed things up

--- migrations/1755795450.sh
+++ migrations/1755795450.sh
@@ -1,3 +1,3 @@
 echo "Ensure latest uwsm is installed"
 
-sudo pacman -Syu --noconfirm uwsm
+sudo pacman -Syu --noconfirm --needed uwsm
--- migrations/1756360552.sh
+++ migrations/1756360552.sh
@@ -4,4 +4,4 @@ sudo cp /etc/pacman.conf /etc/pacman.conf.bak
 sudo sed -i '/\[omarchy\]/,+2 d' /etc/pacman.conf
 sudo sed -i '/\[chaotic-aur\]/,+2 d' /etc/pacman.conf
 sudo bash -c 'echo -e "\n[omarchy]\nSigLevel = Optional TrustAll\nServer = https://pkgs.omarchy.org/\$arch" >> /etc/pacman.conf'
-sudo pacman -Syu --noconfirm
+sudo pacman -Syu --noconfirm --needed
