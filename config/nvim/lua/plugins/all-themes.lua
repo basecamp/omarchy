280a5231d0344636d19b92ecaa080156f9b4d48e
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2025-09-23 12:25:04 +0200

    Make it possible to hot reload native themes as they are preinstalled, 3rd party themes with a custom neovim.lua file, and 3rd party with no neovim.lua which will use pixel by default

--- config/nvim/lua/plugins/all-themes.lua
+++ config/nvim/lua/plugins/all-themes.lua
@@ -3,48 +3,38 @@ return {
 	-- This ensures all colorschemes are available for hot-reloading
 	{
 		"ribru17/bamboo.nvim",
-		lazy = true,
 	},
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
-		lazy = true,
 	},
 	{
 		"sainnhe/everforest",
-		lazy = true,
 	},
 	{
 		"ellisonleao/gruvbox.nvim",
-		lazy = true,
 	},
 	{
 		"rebelot/kanagawa.nvim",
-		lazy = true,
 	},
 	{
 		"tahayvr/matteblack.nvim",
-		lazy = true,
 	},
 	{
 		"loctvl842/monokai-pro.nvim",
-		lazy = true,
 	},
 	{
 		"shaunsingh/nord.nvim",
-		lazy = true,
 	},
 	{
 		"rose-pine/neovim",
 		name = "rose-pine",
-		lazy = true,
 	},
 	{
 		"folke/tokyonight.nvim",
-		lazy = true,
 	},
 	{
 		"bjarneo/pixel.nvim",
-		lazy = true,
+		name = "pixel",
 	},
 }
