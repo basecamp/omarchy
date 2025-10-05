86b82ebe33e55c23e1f63d7cb735ae157b94d900
David Heinemeier Hansson <david@hey.com>
2025-10-05 21:54:24 +0200

    Simplify migration

--- migrations/1759093827.sh
+++ migrations/1759093827.sh
@@ -1,11 +1,3 @@
 echo "Adding hidden entries for electron apps"
 
-tee ~/.local/share/applications/electron36.desktop >/dev/null <<EOF
-[Desktop Entry]
-Hidden=true
-EOF
-
-tee ~/.local/share/applications/electron37.desktop >/dev/null <<EOF
-[Desktop Entry]
-Hidden=true
-EOF
+cp $OMARCHY_PATH/applications/hidden/electron*.desktop ~/.local/share/applications/
