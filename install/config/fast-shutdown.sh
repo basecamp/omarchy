7d6477f21240221d13879ad51df107bdc2875cda
David Heinemeier Hansson <david@hey.com>
2025-10-05 18:13:47 +0200

    Reduce shutdown timeout from 10s to 5s

--- install/config/fast-shutdown.sh
+++ install/config/fast-shutdown.sh
@@ -2,6 +2,6 @@ sudo mkdir -p /etc/systemd/system.conf.d
 
 cat <<EOF | sudo tee /etc/systemd/system.conf.d/10-faster-shutdown.conf
 [Manager]
-DefaultTimeoutStopSec=10s
+DefaultTimeoutStopSec=5s
 EOF
 sudo systemctl daemon-reload
