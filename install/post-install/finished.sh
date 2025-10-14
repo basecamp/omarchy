54be7db9120623c458910db1f11c85fcb981925a
Jon Kinney <jonkinney@gmail.com>
2025-10-14 13:40:59 -0500

    Restore comment for finished.sh

--- install/post-install/finished.sh
+++ install/post-install/finished.sh
@@ -72,8 +72,8 @@ if sudo test -f /etc/sudoers.d/99-omarchy-installer; then
   echo_in_style "Remember to remove USB installer!"
 fi
 
-# --negative "": no negative button (press Enter to continue style)
-if gum confirm --padding "0 0 0 $((PADDING_LEFT + 32))" --default --affirmative "Reboot Now" --negative "" "" </dev/tty; then
+# Exit gracefully if user chooses not to reboot
+if gum confirm --padding "0 0 0 $((PADDING_LEFT + 32))" --show-help=false --default --affirmative "Reboot Now" --negative "" ""; then
   # Clear screen to hide any shutdown messages
   clear
 
