2f18cf940e3087103e2e89e6b8e06df7970275ce
Victor Jean <victorjean@pm.me>
2025-10-19 10:08:39 +0200

    removed comment

--- default/waybar/indicators/do-not-disturb.sh
+++ default/waybar/indicators/do-not-disturb.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 current_mode=$(makoctl mode | grep "do-not-disturb")
-# echo "Debug: Current mode is '$current_mode'"
 
 if [[ "$current_mode" == "do-not-disturb" ]]; then
   echo '{"text": "", "tooltip": "All notifications are silenced", "class": "active"}'
