6d583028d5530706c87d2f59213f3d858c982141
Ryan Hughes <ryan@heyoodle.com>
2025-10-22 01:19:56 -0400

    Update path

--- install.sh
+++ install.sh
@@ -4,10 +4,9 @@
 set -eEo pipefail
 
 # Define Omarchy locations
-export OMARCHY_PATH="$HOME/.local/share/omarchy"
+export OMARCHY_PATH="/usr/share/omarchy"
 export OMARCHY_INSTALL="$OMARCHY_PATH/install"
 export OMARCHY_INSTALL_LOG_FILE="/var/log/omarchy-install.log"
-export PATH="$OMARCHY_PATH/bin:$PATH"
 
 # Install
 source "$OMARCHY_INSTALL/helpers/all.sh"
