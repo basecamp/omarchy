9eaff36f84ac7a93ff3692ff206ac634ee5d16b8
Miguel Palau Zarza <mpalauzarza@gmail.com>
2025-10-23 18:03:46 -0600

    reduce pwd attempts from 10 to 3

--- install/config/increase-sudo-tries.sh
+++ install/config/increase-sudo-tries.sh
@@ -1,3 +1,3 @@
-# Give the user 10 instead of 3 tries to fat finger their password before lockout
-echo "Defaults passwd_tries=10" | sudo tee /etc/sudoers.d/passwd-tries
+# Give the user 3 tries to enter their password before lockout
+echo "Defaults passwd_tries=3" | sudo tee /etc/sudoers.d/passwd-tries
 sudo chmod 440 /etc/sudoers.d/passwd-tries
