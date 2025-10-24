9ea5c0317131cd2f2f121ffde7525ca7f12c5e24
Miguel Palau Zarza <mpalauzarza@gmail.com>
2025-10-23 18:10:44 -0600

    5 sudo tries

--- install/config/increase-sudo-tries.sh
+++ install/config/increase-sudo-tries.sh
@@ -1,3 +1,3 @@
-# Give the user 3 tries to enter their password before lockout
-echo "Defaults passwd_tries=3" | sudo tee /etc/sudoers.d/passwd-tries
+# Give the user 5 tries to enter their password before lockout
+echo "Defaults passwd_tries=5" | sudo tee /etc/sudoers.d/passwd-tries
 sudo chmod 440 /etc/sudoers.d/passwd-tries
