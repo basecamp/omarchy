663e60f51f897b0c798b14256cf7ea80c7971867
Jack Sovern <jack-sovern@uiowa.edu>
2025-10-28 13:38:45 -0500

    Add automatic detection of an eduroam network and suggest an install of geteduroam to simplify configuration.

--- install/first-run/wifi.sh
+++ install/first-run/wifi.sh
@@ -1,3 +1,24 @@
 if ! ping -c3 -W1 1.1.1.1 >/dev/null 2>&1; then
+
+  INTERFACE=$(iwctl station list | tail -n +5 | awk '{print $2}' | tr -d " \t\n\r")
+  if [[ -z "$INTERFACE" ]]; then
+      notify-send "Error" "No wireless interface found" -u critical
+      exit 1
+  fi
+
+  iwctl station $INTERFACE scan
+  if iwctl station $INTERFACE get-networks | grep -qF "eduroam"; then
+      if ! yay -S --noconfirm geteduroam; then
+          notify-send "Error" "Failed to install geteduroam package" -u critical
+          exit 1
+      fi
+      RESPONSE=$(notify-send --action 'default=Open URL' 'Eduroam Network Detected' 'The eduroam network has been detected. Click here to begin connecting or right click to dismiss.' -u normal -i dialog-information -t 10000)
+      if [[ $RESPONSE == *"default"* ]]; then
+          geteduroam-gui &
+          exit
+      fi
+      yay -R --noconfirm geteduroam
+  fi
+
   notify-send "󰖩    Click to Setup Wi-Fi" "Tab to navigate, Space to select, ? for help." -u critical -t 30000
 fi
