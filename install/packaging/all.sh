4370144b46598d60867f90028ae3663d64ddeddf
SerrpentDagger <serpentdagger.buisness@gmail.com>
2025-12-04 22:42:35 +0100

    Another fix attempt for gum display during install

--- install/packaging/all.sh
+++ install/packaging/all.sh
@@ -1,13 +1,20 @@
 stop_log_output
-if gum confirm 'Install all default Omarchy apps?' --negative 'Customise'; then
-  start_log_output
+gum confirm 'Install all default Omarchy apps?' --negative 'Customise'; confirmed=$?
+start_log_output
+if [[ $confirmed == 0 ]]; then
   run_logged $OMARCHY_INSTALL/packaging/base.sh
 else
-  source $OMARCHY_INSTALL/packaging/custom.sh
-  start_log_output
+  run_logged $OMARCHY_INSTALL/packaging/custom.sh
 fi
+
 run_logged $OMARCHY_INSTALL/packaging/fonts.sh
 run_logged $OMARCHY_INSTALL/packaging/nvim.sh
 run_logged $OMARCHY_INSTALL/packaging/icons.sh
-gum confirm 'Install all default Omarchy webapps? (Can be easily added/removed later in Omarchy menu)' && run_logged $OMARCHY_INSTALL/packaging/webapps.sh
+
+stop_log_output
+gum confirm 'Install all default Omarchy webapps? (Can be easily added/removed later in Omarchy menu)'; confirmed=$?
+start_log_output
+if [[ $confirmed == 0 ]]; then
+  run_logged $OMARCHY_INSTALL/packaging/webapps.sh
+fi
 run_logged $OMARCHY_INSTALL/packaging/tuis.sh
--- install/packaging/select-install.sh
+++ install/packaging/select-install.sh
@@ -16,13 +16,13 @@ fzf_args=(
   --color 'pointer:green,marker:green'
 )
 
+stop_log_output
 pkg_names=$(grep -v '^#' "$OMARCHY_INSTALL/$1" | grep -v '^$' | fzf "${fzf_args[@]}")
+start_log_output
 
 if [[ -n "$pkg_names" ]]; then # If nonempty selection.
   # Convert newline-separated selections to space-separated for yay
-  start_log_output
   echo "$pkg_names" | tr '\n' ' ' | xargs sudo pacman -S --noconfirm --needed
-  stop_log_output
 #  echo 'Selected following from file:'
 #  echo "$pkg_names"
 #else
