54309d6471f58a2a26f2a777ec5e9f9f04778f16
SerrpentDagger <serpentdagger.buisness@gmail.com>
2025-12-04 22:55:03 +0100

    Rearranged to avoid erroring with negative choice in gum.

--- install/packaging/all.sh
+++ install/packaging/all.sh
@@ -1,9 +1,9 @@
 stop_log_output
-gum confirm 'Install all default Omarchy apps?' --negative 'Customise'; confirmed=$?
-start_log_output
-if [[ $confirmed == 0 ]]; then
+if gum confirm 'Install all default Omarchy apps?' --negative 'Customise'; then
+  start_log_output
   run_logged $OMARCHY_INSTALL/packaging/base.sh
 else
+  start_log_output
   run_logged $OMARCHY_INSTALL/packaging/custom.sh
 fi
 
@@ -12,9 +12,10 @@ run_logged $OMARCHY_INSTALL/packaging/nvim.sh
 run_logged $OMARCHY_INSTALL/packaging/icons.sh
 
 stop_log_output
-gum confirm 'Install all default Omarchy webapps? (Can be easily added/removed later in Omarchy menu)'; confirmed=$?
-start_log_output
-if [[ $confirmed == 0 ]]; then
+if gum confirm 'Install all default Omarchy webapps? (Can be easily added/removed later in Omarchy menu)'; then
+  start_log_output
   run_logged $OMARCHY_INSTALL/packaging/webapps.sh
+else
+  start_log_output
 fi
 run_logged $OMARCHY_INSTALL/packaging/tuis.sh
