03f8bcbfaa57eb8e440b7e140ed0039607dd4dbe
SerrpentDagger <serpentdagger.buisness@gmail.com>
2025-12-05 18:27:02 +0100

    sudo is bad for yay. :(

--- install/packaging/from-file-aur.sh
+++ install/packaging/from-file-aur.sh
@@ -8,6 +8,6 @@ if [[ -f "$OMARCHY_INSTALL/$1" ]]; then
   mapfile -t packages < <(grep -v '^#' "$OMARCHY_INSTALL/$1" | grep -v '^$')
   if [[ ${#packages[@]} -ne 0 ]]; then
     # Install all of them
-    sudo yay -S --noconfirm --needed "${packages[@]}"
+    yay -S --noconfirm --needed "${packages[@]}"
   fi
 fi
