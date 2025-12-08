45abf4d491c47d33f82e9f9c35f245eebc01845a
Pedro Pinto <git@pedropinto.me>
2025-12-08 13:14:12 +0000

    Update logic for tooltip-format
    Since tooltip-format is present by default we need to check if it's
    either absent or still as default.

--- migrations/1765198150.sh
+++ migrations/1765198150.sh
@@ -1,18 +1,25 @@
 echo "Display update version number and allow right-click to open release notes"
 
-WAYBAR_CONFIG="$HOME/.config/waybar/config_test.jsonc"
+WAYBAR_CONFIG="$HOME/.config/waybar/config.jsonc"
 
-# Check only inside custom/update for on-click-right
 if ! sed -n '/"custom\/update": {/,/},/p' "$WAYBAR_CONFIG" | grep -q '"on-click-right"'; then
   sed -i '/"custom\/update": {/,/},/ {
     /"exec":/a\    "on-click-right": "xdg-open https://github.com/basecamp/omarchy/releases",
   }' "$WAYBAR_CONFIG"
 fi
 
-# Check only inside custom/update for tooltip-format
-if ! sed -n '/"custom\/update": {/,/},/p' "$WAYBAR_CONFIG" | grep -q '"tooltip-format"'; then
+UPDATE_BLOCK=$(sed -n '/"custom\/update": {/,/},/p' "$WAYBAR_CONFIG")
+
+if ! grep -q '"tooltip-format"' <<<"$UPDATE_BLOCK"; then
+  # Add if missing
   sed -i '/"custom\/update": {/,/},/ {
     /"exec":/a\    "tooltip-format": "{text}",
+  }' "$WAYBAR_CONFIG"
+
+elif grep -q '"tooltip-format": "Omarchy update available"' <<<"$UPDATE_BLOCK"; then
+  # Replace if default
+  sed -i '/"custom\/update": {/,/},/ {
+    s/"tooltip-format": "Omarchy update available"/"tooltip-format": "{text}"/
   }' "$WAYBAR_CONFIG"
 fi
 
