23034cfa00f71df860a14163ea528ba0ce78d9c1
Horace Ko <horace.ko@gmail.com>
2025-12-09 12:19:48 +0800

    Write migration to remove the old symlink config and copy over the new hard file config.

---
+++ migrations/1764378788.sh
@@ -0,0 +1,9 @@
+echo "Remove direct symlink to Mako config in theme, and copy over the new Mako config."
+
+# If there's a symlink, remove it and copy over the new config file.
+if test -L "~/.config/mako/config"; then
+  rm -f "~/.config/mako/config" && cp "~/.local/share/omarchy/config/mako/config" "~/.config/mako/config"
+# Else, a real config file already exists. Note as such and don't do anything.
+elif test -f "~/.config/mako/config"; then
+  echo "Mako config file already exists, skipping."
+fi
