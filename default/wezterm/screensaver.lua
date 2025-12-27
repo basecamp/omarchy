6b361d2a047f1376d2a243cdfd584f26a0aeca75
broccoli-robbie <broccolirobbie@proton.me>
2025-12-27 11:59:22 -0500

    fix screensaver support for wezterm

--- default/wezterm/screensaver.lua
+++ default/wezterm/screensaver.lua
@@ -1,12 +1,23 @@
-local screensaver = {}
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
 
-function screensaver.apply_to_config(config)
-  config.window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
-  }
-end
+-- Font
+config.font_size = 18
 
-return screensaver
+-- Window
+config.window_close_confirmation = "NeverPrompt"
+config.window_decorations = "NONE"
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+config.colors = {
+  background = "black",
+  cursor_bg = "black",
+}
+config.window_background_opacity = 1.0
+config.enable_tab_bar = false
+
+return config
