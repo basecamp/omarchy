fb6d061d5ded19422105705dcad971dd9fb5a230
David Heinemeier Hansson <david@hey.com>
2025-12-30 16:18:20 -0800

    Migrate the scrolltouchpad setting in userland too

---
+++ migrations/1767138576.sh
@@ -0,0 +1,6 @@
+echo "Update terminal scrolltouchpad setting to Hyprland 0.53 style"
+
+if grep -q "scrolltouchpad" ~/.config/hypr/input.conf; then
+  sed -Ei 's/^windowrule = scrolltouchpad ([^,]+), class:\(([^)]+)\)$/windowrule = match:class (\2), scroll_touchpad \1/;
+  s/^windowrule = scrolltouchpad ([^,]+), class:([^ ]+)$/windowrule = match:class \2, scroll_touchpad \1/' ~/.config/hypr/input.conf
+fi
