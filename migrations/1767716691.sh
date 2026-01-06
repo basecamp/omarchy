06d55e3e7300d492502f921f5abbb0ca841009b2
Pierre Olivier Martel <pomartel@coderubik.com>
2026-01-06 11:33:19 -0500

    Add icons for additional audio profiles in Waybar

---
+++ migrations/1767716691.sh
@@ -0,0 +1,19 @@
+echo "Add icons for additional audio profiles in Waybar"
+
+if ! grep -q '"headphone": ""' "$HOME/.config/waybar/config.jsonc"; then
+  sed -i '
+    /"pulseaudio": {/,/^[ ]*}/{
+      /"format-icons": {/,/^[ ]*}/{
+        /"default":/i\
+\      "headphone": "",\
+\      "hands-free": "",\
+\      "headset": "",\
+\      "phone": "",\
+\      "portable": "",\
+\      "car": "",
+      }
+    }
+  ' "$HOME/.config/waybar/config.jsonc"
+
+  omarchy-restart-waybar
+fi
