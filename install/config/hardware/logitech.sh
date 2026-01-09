b6868befb3c2a614c6960bd9e7c6600957eeedd0
Axel Fontaine <axel@axelfontaine.com>
2026-01-09 09:41:21 +0100

    Removed lsusb dependency

--- install/config/hardware/logitech.sh
+++ install/config/hardware/logitech.sh
@@ -1,11 +1,11 @@
 # This sets up the special keys on Logitech keyboards
-sudo pacman -S --noconfirm --needed usbutils
-# Check if we have a Logitech Bolt receiver
-if lsusb | grep -q '046d:c548 Logitech, Inc. Logi Bolt Receiver'; then
-    echo "Setting up Logitech Bolt receiver..."
-    sudo pacman -S --needed --noconfirm solaar
-    mkdir -p ~/.config/solaar
-    cat <<EOF > ~/.config/solaar/rules.yaml
+if grep -q 'Logitech USB Receiver' /proc/bus/input/devices; then
+  echo "Detected Logitech USB Receiver"
+
+  sudo pacman -S --needed --noconfirm solaar
+
+  mkdir -p ~/.config/solaar
+  cat <<EOF > ~/.config/solaar/rules.yaml
 %YAML 1.3
 ---
 - Key: [Emoji, pressed]
@@ -20,26 +20,25 @@ if lsusb | grep -q '046d:c548 Logitech, Inc. Logi Bolt Receiver'; then
 - Execute: $HOME/.local/share/omarchy/bin/omarchy-lock-screen
 ...
 EOF
-    sudo solaar config "MX Keys S" divert-keys "Emoji" Diverted
-    sudo solaar config "MX Keys S" divert-keys "Screen Capture" Diverted
-    sudo solaar config "MX Keys S" divert-keys "Screen Lock" Diverted
 
-    cat <<EOF > ~/.config/systemd/user/solaar.service
+  sudo solaar config "MX Keys S" divert-keys "Emoji" Diverted
+  sudo solaar config "MX Keys S" divert-keys "Screen Capture" Diverted
+  sudo solaar config "MX Keys S" divert-keys "Screen Lock" Diverted
+
+  cat <<EOF > ~/.config/systemd/user/solaar.service
 [Unit]
 Description=Solaar
 After=graphical-session.target
 
 [Service]
 Type=simple
-ExecStart=/usr/bin/solaar -w hide
+ExecStart=/usr/bin/solaar -w hide -b solaar
 Environment=DISPLAY=:0
 
 [Install]
 WantedBy=default.target
 EOF
-    systemctl --user daemon-reload
-    systemctl --user enable solaar.service
-    systemctl --user start solaar.service
-else
-    echo "No Logitech Bolt receiver found."
+
+  systemctl --user daemon-reload
+  systemctl --user enable solaar.service
 fi
