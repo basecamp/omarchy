a2111c21bc1945e781d5b1cd7300e361825647f8
Jon Kinney <jonkinney@gmail.com>
2026-01-24 23:42:56 -0600

    Fix dns, wifi, bluetooth for pi 500+

--- install/config/all.sh
+++ install/config/all.sh
@@ -35,6 +35,7 @@ run_logged $OMARCHY_INSTALL/config/hardware/nvidia.sh
 run_logged $OMARCHY_INSTALL/config/hardware/vulkan.sh
 run_logged $OMARCHY_INSTALL/config/hardware/fix-f13-amd-audio-input.sh
 run_logged $OMARCHY_INSTALL/config/hardware/fix-bcm43xx.sh
+run_logged $OMARCHY_INSTALL/config/hardware/fix-pi500-wifi.sh
 run_logged $OMARCHY_INSTALL/config/hardware/fix-apple-spi-keyboard.sh
 run_logged $OMARCHY_INSTALL/config/hardware/fix-apple-suspend-nvme.sh
 run_logged $OMARCHY_INSTALL/config/hardware/fix-apple-t2.sh
