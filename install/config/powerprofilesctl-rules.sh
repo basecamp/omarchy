e8caa272872239cd044368d7ef170b3d7e10b91c
Pierre Olivier Martel <pomartel@coderubik.com>
2026-01-28 13:33:09 -0500

    Check for battery presence before installing power profile rules

--- install/config/powerprofilesctl-rules.sh
+++ install/config/powerprofilesctl-rules.sh
@@ -1,7 +1,19 @@
-sudo mkdir -p /etc/udev/rules.d
+battery_present() {
+  for bat in /sys/class/power_supply/BAT*; do
+    [[ -r "$bat/present" ]] &&
+    [[ "$(cat "$bat/present")" == "1" ]] &&
+    [[ "$(cat "$bat/type")" == "Battery" ]] &&
+    return 0
+  done
+  return 1
+}
 
-cat <<'EOF' | sudo tee "/etc/udev/rules.d/99-power-profile.rules"
+if battery_present; then
+  sudo mkdir -p /etc/udev/rules.d
+
+  cat <<'EOF' | sudo tee "/etc/udev/rules.d/99-power-profile.rules"
 SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="0", RUN+="/usr/bin/powerprofilesctl set power-saver"
 SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="1", RUN+="/usr/bin/powerprofilesctl set balanced"
 EOF
-sudo udevadm control --reload
+  sudo udevadm control --reload
+fi
