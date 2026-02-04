b2358b4922764ffe2eed2a1bc48fe65b5c6626c4
Sameer Paudel <paudelsameer888@gmail.com>
2026-02-04 17:58:34 +0545

    Enable Synaptics InterTouch for confirmed touchpads if not already loaded

---
+++ install/config/hardware/synaptic-touchpad.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Enable Synaptics InterTouch for confirmed touchpads if not already loaded
+
+if ! lsmod | grep -q '^psmouse'; then
+    echo "Loading psmouse module with for synaptics touchpad..."
+    modprobe psmouse synaptics_intertouch=1
+else
+    echo "psmouse module loaded."
+fi
