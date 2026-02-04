910be247d4eaa5ce8e1231467d5bd1a7b1604af1
Sameer Paudel <83913671+Sameer292@users.noreply.github.com>
2026-02-04 18:06:41 +0545

    fix echo message

--- install/config/hardware/synaptic-touchpad.sh
+++ install/config/hardware/synaptic-touchpad.sh
@@ -2,7 +2,7 @@
 # Enable Synaptics InterTouch for confirmed touchpads if not already loaded
 
 if ! lsmod | grep -q '^psmouse'; then
-    echo "Loading psmouse module with for synaptics touchpad..."
+    echo "Loading psmouse module for synaptics touchpad..."
     modprobe psmouse synaptics_intertouch=1
 else
     echo "psmouse module loaded."
