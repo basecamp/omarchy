4b18e5f260123f64b5379b97a4d75df2ac121c86
Sameer Paudel <paudelsameer888@gmail.com>
2026-02-04 19:19:40 +0545

    remove shebang and echo messages

--- install/config/hardware/synaptic-touchpad.sh
+++ install/config/hardware/synaptic-touchpad.sh
@@ -1,9 +1,5 @@
-#!/bin/bash
 # Enable Synaptics InterTouch for confirmed touchpads if not already loaded
 
 if ! lsmod | grep -q '^psmouse'; then
-    echo "Loading psmouse module for synaptics touchpad..."
-    modprobe psmouse synaptics_intertouch=1
-else
-    echo "psmouse module loaded."
+  modprobe psmouse synaptics_intertouch=1
 fi
