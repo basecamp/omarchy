165fa4d06e5034e380f1ae169ffd5d6697d4f867
Sameer Paudel <paudelsameer888@gmail.com>
2026-02-04 20:43:21 +0545

    check for synaptics intertouch and enable it only if available

--- install/config/hardware/synaptic-touchpad.sh
+++ install/config/hardware/synaptic-touchpad.sh
@@ -1,5 +1,6 @@
 # Enable Synaptics InterTouch for confirmed touchpads if not already loaded
 
-if ! lsmod | grep -q '^psmouse'; then
-  modprobe psmouse synaptics_intertouch=1
+if grep -qi synaptics /proc/bus/input/devices \
+   && ! lsmod | grep -q '^psmouse'; then
+    modprobe psmouse synaptics_intertouch=1
 fi
