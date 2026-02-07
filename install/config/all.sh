48497264271c8747ede2b1978a052c8fe9151926
Chip Koziara <chipkoziara@users.noreply.github.com>
2026-02-07 10:42:01 -0500

    Add Framework Desktop ARGB fan theme syncing

--- install/config/all.sh
+++ install/config/all.sh
@@ -40,3 +40,4 @@ run_logged $OMARCHY_INSTALL/config/hardware/fix-asus-rog-audio-mixer.sh
 run_logged $OMARCHY_INSTALL/config/hardware/fix-asus-rog-mic.sh
 run_logged $OMARCHY_INSTALL/config/hardware/fix-yt6801-ethernet-adapter.sh
 run_logged $OMARCHY_INSTALL/config/hardware/fix-synaptic-touchpad.sh
+run_logged $OMARCHY_INSTALL/config/hardware/framework-desktop-argb.sh
