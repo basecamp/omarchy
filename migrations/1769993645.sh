a8499ca87a1ef4c782e4d764bd80b8cbac2872ab
severin-richner <severin.richner@gmail.com>
2026-02-07 23:44:42 +0100

    Migration adds the new hyprland config file to the users hyprland.conf.

--- migrations/1769993645.sh
+++ migrations/1769993645.sh
@@ -1,3 +1,14 @@
-echo "Resetting theme"
+echo "Adding gum env config to hyprland.conf"
+HYPRLAND_CONFIG=~/.config/hypr/hyprland.conf
 
+if [[ -f $HYPRLAND_CONFIG ]]; then
+	if ! grep -qx 'source = ~/.config/omarchy/current/theme/gum.env.conf' "$HYPRLAND_CONFIG"; then
+		sed -i --follow-symlinks '/^source = ~\/\.config\/omarchy\/current\/theme\/hyprland\.conf$/a source = ~/.config/omarchy/current/theme/gum.env.conf' "$HYPRLAND_CONFIG"
+	fi
+    if ! grep -qx 'source = ~/.config/omarchy/current/theme/gum.env.conf' "$HYPRLAND_CONFIG"; then
+    	echo "Error: Failed to add 'source = ~/.config/omarchy/current/theme/gum.env.conf' to $HYPRLAND_CONFIG."
+    fi
+fi
+
+echo "Resetting theme"
 omarchy-theme-reset
