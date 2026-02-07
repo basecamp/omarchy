c87fba9f70f3c1c30224d2a3a9315319473217da
Bjarne Øverli <bjarne.oeverli@gmail.com>
2026-02-07 14:25:16 +0100

    Format the object properly

--- themes/vantablack/neovim.lua
+++ themes/vantablack/neovim.lua
@@ -2,7 +2,10 @@ return {
   {
     "bjarneo/vantablack.nvim",
     priority = 1000,
-    dependencies = { "bjarneo/aether.nvim", branch = "v2" },
+    dependencies = {
+      "bjarneo/aether.nvim",
+      branch = "v2"
+    },
   },
   {
     "LazyVim/LazyVim",
