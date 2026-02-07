5c0e8ac7dc9d27cb0c698315bf9338a32b95c6c5
severin-richner <severin.richner@gmail.com>
2026-02-07 14:27:16 +0100

    Fix migration script.

--- migrations/1769993645.sh
+++ migrations/1769993645.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-# Reload hyprland to add the Gum environment variables.
-omarchy-restart-hyprctl
+# reapply the current theme and restart hyprland to ensure the environment variables are set.
+THEME_NAME_PATH="$HOME/.config/omarchy/current/theme.name"
+
+if [[ -f $THEME_NAME_PATH ]]; then
+  omarchy-theme-set "$(cat $THEME_NAME_PATH)"
+  omarchy-restart-hyprctl
+fi
