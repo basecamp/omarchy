a0a9d3fcc83409e8db5d3cc83bf1ffcf8ddd9e3c
severin-richner <severin.richner@gmail.com>
2026-02-07 17:00:13 +0100

    Add omarchy-theme-reset utility.

--- migrations/1769993645.sh
+++ migrations/1769993645.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# reapply the current theme and restart hyprland to ensure the environment variables are set.
-THEME_NAME_PATH="$HOME/.config/omarchy/current/theme.name"
-
-if [[ -f $THEME_NAME_PATH ]]; then
-  omarchy-theme-set "$(cat $THEME_NAME_PATH)"
-  omarchy-restart-hyprctl
-fi
+# reapply the current theme (also restarts hyprland to add the gum env variables)
+omarchy-theme-reset
