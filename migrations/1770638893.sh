71974b2cb182de3c6104f72a50aa2d886dd9ff1d
David Heinemeier Hansson <david@hey.com>
2026-02-09 22:59:01 +0100

    Fix

--- migrations/1770638893.sh
+++ migrations/1770638893.sh
@@ -4,6 +4,6 @@ omarchy-pkg-add tmux
 
 if [[ ! -f ~/.config/tmux/tmux.conf ]]; then
   mkdir -p ~/.config/tmux
-  cp $OMARCHY/config/tmux/tmux.config ~/.config/tmux/tmux.conf
-  omarchy-theme-reset
+  cp $OMARCHY_PATH/config/tmux/tmux.conf ~/.config/tmux/tmux.conf
+  omarchy-theme-refresh
 fi
