f8b0e7205440dfae65eb8a1b96e0e8834d2bac0a
Pierre Olivier Martel <pomartel@coderubik.com>
2026-02-11 21:31:12 -0500

    put it at the top

--- migrations/1770728860.sh
+++ migrations/1770728860.sh
@@ -1,6 +1,5 @@
 echo "Unbind ctrl+shift+left/right in Ghostty so it doesn't interfere with tmux keybindings"
 
 if ! grep -q "keybind = ctrl+shift+left=" ~/.config/ghostty/config; then
-  sed -i '/keybind = super+control+shift+alt+arrow_right=resize_split:right,100/a\keybind = ctrl+shift+left=unbind\nkeybind = ctrl+shift+right=unbind' ~/.config/ghostty/config
+  sed -i '/# Keyboard bindings/a\keybind = ctrl+shift+left=unbind\nkeybind = ctrl+shift+right=unbind' ~/.config/ghostty/config
 fi
-
