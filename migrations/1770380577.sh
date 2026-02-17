7e991f33a4a218e0790a6c8012c09f986f78348e
David Heinemeier Hansson <david@hey.com>
2026-02-17 12:25:47 +0100

    Fix migration

--- migrations/1770380577.sh
+++ migrations/1770380577.sh
@@ -1,4 +1,5 @@
-#link the visual background selector menu config
+echo "Use interactive background selector menu"
 
 mkdir -p ~/.config/elephant/menus
 ln -snf $OMARCHY_PATH/default/elephant/omarchy_background_selector.lua ~/.config/elephant/menus/omarchy_background_selector.lua
+omarchy-restart-walker
