e24a5d4120027fe5ccfc3e81d62d84c91511fe74
Steve Godlewski <steve.w.godlewski@gmail.com>
2026-02-18 12:19:35 -0700

    call the install file from the migration file

--- migrations/1770483021.sh
+++ migrations/1770483021.sh
@@ -1,11 +1,4 @@
 echo "Install Framework 16 keyboard RGB support"
 
-if omarchy-hw-framework16; then
-  omarchy-pkg-add qmk-hid
-
-  if [[ ! -f /etc/udev/rules.d/50-framework16-qmk-hid.rules ]]; then
-    sudo cp "$OMARCHY_PATH/default/udev/framework16-qmk-hid.rules" /etc/udev/rules.d/50-framework16-qmk-hid.rules
-    sudo udevadm control --reload-rules
-    sudo udevadm trigger
-  fi
-fi
+source $OMARCHY_PATH/install/packaging/framework16.sh
+source $OMARCHY_PATH/install/config/hardware/framework16-qmk-hid.sh
