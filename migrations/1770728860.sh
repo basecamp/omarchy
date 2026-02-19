35aee2dd4459da89e42333db7e34b45ea6d9bbc1
Pierre Olivier Martel <pomartel@coderubik.com>
2026-02-18 21:16:16 -0500

    Add guard clause

--- migrations/1770728860.sh
+++ migrations/1770728860.sh
@@ -1,5 +1,5 @@
 echo "Unbind ctrl+shift+page_up/page_down in Ghostty so it doesn't interfere with tmux keybindings"
 
-if ! grep -q "keybind = ctrl+shift+page_up=" ~/.config/ghostty/config; then
+if [[ -f ~/.config/ghostty/config ]] && ! grep -q "keybind = ctrl+shift+page_up=" ~/.config/ghostty/config; then
   sed -i '/# Keyboard bindings/a\keybind = ctrl+shift+page_up=unbind\nkeybind = ctrl+shift+page_down=unbind' ~/.config/ghostty/config
 fi
