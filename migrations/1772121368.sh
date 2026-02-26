1f6ec3a7c6d4845ecb05695e3e9c91dd7ce20c07
Gavin Moceri <chmcs@proton.me>
2026-02-26 11:44:27 -0500

    Theme plymouth boot splash and encryption screen on theme change

--- migrations/1772121368.sh
+++ migrations/1772121368.sh
@@ -1,9 +1,5 @@
-if [[ $(plymouth-set-default-theme) != "omarchy" ]]; then
-  sudo cp -r "$HOME/.local/share/omarchy/default/plymouth" /usr/share/plymouth/themes/omarchy/
-  sudo plymouth-set-default-theme omarchy
-fi
+echo "Install plymouth theme color support"
 
-# Install plymouth theme color helper for passwordless theme updates
 sudo cp "$OMARCHY_PATH/bin/omarchy-plymouth-update-theme" /usr/local/bin/omarchy-plymouth-update-theme
 sudo chmod 755 /usr/local/bin/omarchy-plymouth-update-theme
 sudo chown root:root /usr/local/bin/omarchy-plymouth-update-theme
