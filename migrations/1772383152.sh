e4aee248a52d4a521648d191a794c788772c84e1
Pierre Olivier Martel <pomartel@coderubik.com>
2026-03-02 17:09:10 -0500

    two slashes

--- migrations/1772383152.sh
+++ migrations/1772383152.sh
@@ -1,6 +1,6 @@
 echo "Enable power saver profile on low battery for laptops"
 
-if omarchy-battery-present && [[ ! -f ~/.config/systemd/user//omarchy-powerprofile-low-battery.service ]]; then
+if omarchy-battery-present && [[ ! -f ~/.config/systemd/user/omarchy-powerprofile-low-battery.service ]]; then
   mkdir -p ~/.config/systemd/user
 
   cp $OMARCHY_PATH/config/systemd/user/omarchy-powerprofile-low-battery.* ~/.config/systemd/user/
