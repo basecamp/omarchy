35295359db465104bb6b96fff35256b9430fdc43
Sam Simons <47101903+kromsam@users.noreply.github.com>
2026-03-06 15:50:48 +0100

    Remove redundant mkdir command for NetworkManager

--- install/config/hardware/network.sh
+++ install/config/hardware/network.sh
@@ -3,7 +3,6 @@ sudo systemctl enable iwd.service
 
 # Configure NetworkManager
 sudo mkdir -p /etc/NetworkManager/conf.d
-sudo mkdir -p /etc/NetworkManager/conf.d
 sudo cp -f ~/.local/share/omarchy/default/networkmanager/omarchy_networkmanager.conf /etc/NetworkManager/conf.d/omarchy_networkmanager.conf
 
 # Prevent systemd-networkd-wait-online timeout on boot
