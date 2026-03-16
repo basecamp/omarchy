75fb1db81b47f82754531e874e4f8e8ed35b65c6
Adam Roth <adamroth598@gmail.com>
2026-03-16 22:46:40 +0100

    Enable networkd during nettui migration

--- migrations/1773693740.sh
+++ migrations/1773693740.sh
@@ -4,6 +4,10 @@ if omarchy-cmd-missing nettui; then
   omarchy-pkg-add nettui
 fi
 
+sudo systemctl enable --now systemd-networkd.service
+sudo systemctl disable systemd-networkd-wait-online.service
+sudo systemctl mask systemd-networkd-wait-online.service
+
 if omarchy-cmd-present impala; then
   omarchy-pkg-drop impala
 fi
