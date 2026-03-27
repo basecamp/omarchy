e21e24392c3e861b79153bc0feeb61bf76b4aee9
Kaleb Dison <121968849+kaldis96@users.noreply.github.com>
2026-03-27 18:14:40 +0000

    Update webapps.sh to include jellyfin, personal github directory, and twitch

--- install/packaging/webapps.sh
+++ install/packaging/webapps.sh
@@ -1,4 +1,6 @@
 omarchy-webapp-install "YouTube" https://youtube.com/ YouTube.png
-omarchy-webapp-install "GitHub" https://github.com/ GitHub.png
+omarchy-webapp-install "GitHub" https://github.com/kaldis96 GitHub.png
 omarchy-webapp-install "Figma" https://figma.com/ Figma.png
 omarchy-webapp-install "Discord" https://discord.com/channels/@me Discord.png
+omarchy-webapp-install "Jellyfin" http://71.12.195.157:60377/ Jellyfin.png
+omarchy-webapp-install "Twitch" https://twitch.tv/directory/following Twitch.png
