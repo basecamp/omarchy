9987f683e4adea30ac83f4e12033d3f19dbcf453
Tony Messias <tonysm@hey.com>
2026-03-28 00:17:28 -0300

    Fix udev RUN commands to use systemd-run
    I was getting errors in the powersaver udev rule handling script. I
    think it's because udev runs in sandbox mode, so it can't access the
    scripts in /home/{user}/...
    
    This change hands-off the scripts to systemd-run, which will run them
    outside the sandbox mode. The --no-block returns immediatelly so it
    doesn't block udev event processing.
    
    Here's the error I was getting:
    
    Mar 27 23:42:10 [REDACTED] (udev-worker)[567]: AC: Process '/usr/bin/powerprofilesctl set balanced' failed with exit code 2.
    Mar 27 23:42:10 [REDACTED] (udev-worker)[567]: AC: Process '/home/[REDACTED]/.local/share/omarchy/bin/omarchy-wifi-powersave on' failed with exit code 237.
    
    With the command:
    
    journalctl -u systemd-udevd -e

--- install/config/powerprofilesctl-rules.sh
+++ install/config/powerprofilesctl-rules.sh
@@ -12,8 +12,8 @@ if omarchy-battery-present; then
     battery_profile="${profiles[1]}"
 
     cat <<EOF | sudo tee "/etc/udev/rules.d/99-power-profile.rules"
-SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="0", RUN+="/usr/bin/powerprofilesctl set $battery_profile"
-SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="1", RUN+="/usr/bin/powerprofilesctl set $ac_profile"
+SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="0", RUN+="/usr/bin/systemd-run --no-block /usr/bin/powerprofilesctl set $battery_profile"
+SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="1", RUN+="/usr/bin/systemd-run --no-block /usr/bin/powerprofilesctl set $ac_profile"
 EOF
 
     sudo udevadm control --reload
