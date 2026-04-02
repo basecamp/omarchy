34caa7b9b953a5e31e80f6cbb2451e53aabd8af4
David Heinemeier Hansson <david@hey.com>
2026-04-02 11:06:38 +0200

    Make sure it's clear to all what migration is being run (even if it doesn't apply to them)

--- migrations/1775059710.sh
+++ migrations/1775059710.sh
@@ -1,6 +1,6 @@
-if lspci -nn | grep -q "106b:180[12]" && [[ ! -f /etc/t2fand.conf ]]; then
-  echo "Installing tuned fan curve for T2 MacBook"
+echo "Install tuned fan curve for T2 MacBook"
 
+if lspci -nn | grep -q "106b:180[12]" && [[ ! -f /etc/t2fand.conf ]]; then
   cat <<EOF | sudo tee /etc/t2fand.conf >/dev/null
 [Fan1]
 low_temp=55
