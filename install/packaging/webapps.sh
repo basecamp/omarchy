83c1b8d38550fac860c2d401c35b1da4d6921dcc
Andrii Furmanets <furmanets.andriy@gmail.com>
2026-05-02 08:47:49 +0300

    Remove only preinstalled webapps

--- install/packaging/webapps.sh
+++ install/packaging/webapps.sh
@@ -1,15 +1,17 @@
-omarchy-webapp-install "HEY" https://app.hey.com HEY.png "omarchy-webapp-handler-hey %u" "x-scheme-handler/mailto"
-omarchy-webapp-install "Basecamp" https://launchpad.37signals.com Basecamp.png
-omarchy-webapp-install "WhatsApp" https://web.whatsapp.com/ WhatsApp.png
-omarchy-webapp-install "Google Photos" https://photos.google.com/ "Google Photos.png"
-omarchy-webapp-install "Google Contacts" https://contacts.google.com/ "Google Contacts.png"
-omarchy-webapp-install "Google Messages" https://messages.google.com/web/conversations "Google Messages.png"
-omarchy-webapp-install "Google Maps" https://maps.google.com "Google Maps.png"
-omarchy-webapp-install "ChatGPT" https://chatgpt.com/ ChatGPT.png
-omarchy-webapp-install "YouTube" https://youtube.com/ YouTube.png
-omarchy-webapp-install "GitHub" https://github.com/ GitHub.png
-omarchy-webapp-install "X" https://x.com/ X.png
-omarchy-webapp-install "Figma" https://figma.com/ Figma.png
-omarchy-webapp-install "Discord" https://discord.com/channels/@me Discord.png
-omarchy-webapp-install "Zoom" https://app.zoom.us/wc/home Zoom.png "omarchy-webapp-handler-zoom %u" "x-scheme-handler/zoommtg;x-scheme-handler/zoomus"
-omarchy-webapp-install "Fizzy" https://app.fizzy.do/ Fizzy.png
+source "$OMARCHY_PATH/install/packaging/webapps-data.sh"
+
+for webapp in "${OMARCHY_PREINSTALLED_WEBAPPS[@]}"; do
+  IFS="|" read -r app_name app_url icon_ref custom_exec mime_types <<< "$webapp"
+
+  args=("$app_name" "$app_url" "$icon_ref")
+
+  if [[ -n $custom_exec || -n $mime_types ]]; then
+    args+=("$custom_exec")
+  fi
+
+  if [[ -n $mime_types ]]; then
+    args+=("$mime_types")
+  fi
+
+  omarchy-webapp-install "${args[@]}"
+done
