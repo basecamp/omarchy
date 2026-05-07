e17fd1129b3993cece52cd53a4e74efbd7e28450
Pierre Olivier Martel <pomartel@coderubik.com>
2026-05-07 11:19:46 -0400

    Simplify the Ghostty fix

--- migrations/1776558973.sh
+++ migrations/1776558973.sh
@@ -3,3 +3,7 @@ echo "Install nautilus-open-any-terminal"
 omarchy-pkg-add nautilus-open-any-terminal
 
 source $OMARCHY_PATH/install/config/nautilus-open-any-terminal.sh
+
+if [[ -f ~/.config/ghostty/config ]] && ! grep -q "^gtk-single-instance" ~/.config/ghostty/config; then
+  sed -i "/^gtk-toolbar-style/a gtk-single-instance = false" ~/.config/ghostty/config
+fi
