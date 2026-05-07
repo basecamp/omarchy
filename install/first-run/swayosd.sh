10908fd193683702ee4bdd03b7fe7ae8173076ce
David Heinemeier Hansson <david@hey.com>
2026-05-07 19:37:20 +0200

    We don't need the input backend (as we manage the hotkeys in Hyprland)

--- install/first-run/swayosd.sh
+++ install/first-run/swayosd.sh
@@ -1,4 +1,5 @@
-sudo systemctl enable --now swayosd-libinput-backend.service
+mkdir -p ~/.config/systemd/user
+cp "$OMARCHY_PATH/config/systemd/user/swayosd-server.service" ~/.config/systemd/user/swayosd-server.service
 
 systemctl --user daemon-reload
 systemctl --user enable --now swayosd-server.service
