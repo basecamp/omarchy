a87d462e901624c70447eca34fb96d2714d9feeb
David Heinemeier Hansson <david@hey.com>
2026-05-07 19:39:09 +0200

    Not needed

--- install/first-run/swayosd.sh
+++ install/first-run/swayosd.sh
@@ -1,5 +1,2 @@
-mkdir -p ~/.config/systemd/user
-cp "$OMARCHY_PATH/config/systemd/user/swayosd-server.service" ~/.config/systemd/user/swayosd-server.service
-
 systemctl --user daemon-reload
 systemctl --user enable --now swayosd-server.service
