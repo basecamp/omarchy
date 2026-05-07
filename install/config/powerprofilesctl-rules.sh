bd989274096d8819976dc402ecbdf9089fc63979
Pierre Olivier Martel <pomartel@coderubik.com>
2026-05-07 10:43:56 -0400

    run as the user

--- install/config/powerprofilesctl-rules.sh
+++ install/config/powerprofilesctl-rules.sh
@@ -1,10 +1,12 @@
 if omarchy-battery-present; then
+  user_uid=$(id -u)
   omarchy_path="${OMARCHY_PATH:-$HOME/.local/share/omarchy}"
   run_path="$omarchy_path/bin:/usr/local/sbin:/usr/local/bin:/usr/bin"
+  run_command="/usr/bin/runuser -u $USER -- /usr/bin/env HOME=$HOME OMARCHY_PATH=$omarchy_path XDG_RUNTIME_DIR=/run/user/$user_uid DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$user_uid/bus PATH=$run_path /usr/bin/systemd-run --user --quiet --collect $omarchy_path/bin/omarchy-powerprofiles-set"
 
   cat <<EOF | sudo tee "/etc/udev/rules.d/99-power-profile.rules"
-SUBSYSTEM=="power_supply", ATTR{type}=="Mains", RUN+="/usr/bin/systemd-run --no-block --collect --unit=omarchy-power-profile --setenv=HOME=$HOME --setenv=OMARCHY_PATH=$omarchy_path --setenv=PATH=$run_path --property=After=power-profiles-daemon.service $omarchy_path/bin/omarchy-powerprofiles-set"
-SUBSYSTEM=="power_supply", ATTR{type}=="USB", RUN+="/usr/bin/systemd-run --no-block --collect --unit=omarchy-power-profile --setenv=HOME=$HOME --setenv=OMARCHY_PATH=$omarchy_path --setenv=PATH=$run_path --property=After=power-profiles-daemon.service $omarchy_path/bin/omarchy-powerprofiles-set"
+SUBSYSTEM=="power_supply", ATTR{type}=="Mains", RUN+="$run_command"
+SUBSYSTEM=="power_supply", ATTR{type}=="USB", RUN+="$run_command"
 EOF
 
   sudo systemctl enable power-profiles-daemon
