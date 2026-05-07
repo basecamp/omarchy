b54e954f2906ce8a01d9b9ade98d80f50be3636d
Mateusz Kowalczyk <mateuszmkowalczyk@gmail.com>
2026-05-07 17:27:27 +0200

    Update comment with Non-Arc Panther Lake GPUs mentioned.

--- install/config/hardware/intel/video-acceleration.sh
+++ install/config/hardware/intel/video-acceleration.sh
@@ -1,7 +1,7 @@
 # This installs hardware video acceleration for Intel GPUs
 
 if INTEL_GPU=$(lspci | grep -iE 'vga|3d|display' | grep -i 'intel'); then
-  # HD Graphics / Iris / Xe / Arc use intel-media-driver + VPL
+  # HD Graphics / Iris / Xe / Arc / Non-Arc Panther Lake use intel-media-driver + VPL
   if [[ ${INTEL_GPU,,} =~ (hd\ graphics|uhd\ graphics|xe|iris|arc|panther\ lake) ]]; then
     omarchy-pkg-add intel-media-driver libvpl vpl-gpu-rt
   elif [[ ${INTEL_GPU,,} =~ "gma" ]]; then
