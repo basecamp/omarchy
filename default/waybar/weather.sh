94cf1ab7fe189313ccdb5523eab099d95011f08c
Mike Pullen <mike.pullen@gmail.com>
2026-05-08 16:13:10 -0500

    feat(waybar): Add weather tooltip text

--- default/waybar/weather.sh
+++ default/waybar/weather.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 icon=$(omarchy-weather-icon 2>/dev/null)
+tooltip=$(omarchy-weather-status 2>/dev/null)
 
 if [[ -n $icon ]]; then
-  icon=$(printf '%s' "$icon" | sed 's/["\\]/\\&/g')
-  printf '{"text":"%s"}\n' "$icon"
+    icon=$(printf '%s' "$icon" | sed 's/["\\]/\\&/g')
+    printf '{"text":"%s", "tooltip":"%s"}' "$icon" "$tooltip"
 else
-  printf '{"text":"","class":"unavailable"}\n'
+    printf '{"text":"","class":"unavailable"}\n'
 fi
