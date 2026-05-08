cd4031646f112218778efe87265c559b0be6f834
Diego Rios <darp_0703@outlook.com>
2026-05-08 10:37:18 -0600

    Mark reboot required after MSI ALC892 migration

--- migrations/1778263200.sh
+++ migrations/1778263200.sh
@@ -3,3 +3,7 @@ set -e
 echo "Disable HDA powersave for MSI X370 GAMING PLUS boards with ALC892"
 
 source "$OMARCHY_PATH/install/config/hardware/msi/fix-alc892-jack-hotplug.sh"
+
+if [[ -f /etc/modprobe.d/90-snd-hda-intel-jackfix.conf ]]; then
+  omarchy-state set reboot-required
+fi
