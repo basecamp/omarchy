4e2da8480a42aa713aa063a1ab8b48cab61809fd
David Heinemeier Hansson <david@hey.com>
2026-05-17 19:51:27 +0200

    Standardize all notification sending

--- install/first-run/wifi.sh
+++ install/first-run/wifi.sh
@@ -1,13 +1,13 @@
 notify_update() {
   (
-    action=$(notify-send "    Update System" "$1" -u critical -A "default=Update")
+    action=$(notify-send -a omarchy-action -u critical --hint=string:omarchy-glyph: "Update System" "$1" -A "default=Update")
     [[ $action == "default" ]] && omarchy-launch-floating-terminal-with-presentation omarchy-update
   ) >/dev/null 2>&1 &
 }
 
 notify_wifi() {
   (
-    action=$(notify-send "󰖩    Click to Setup Wi-Fi" "Tab to navigate, Space to select, ? for help." -u critical -A "default=Setup")
+    action=$(notify-send -a omarchy-action -u critical --hint=string:omarchy-glyph:󰖩 "Click to Setup Wi-Fi" "Tab to navigate, Space to select, ? for help." -A "default=Setup")
     [[ $action == "default" ]] && omarchy-launch-wifi
   ) >/dev/null 2>&1 &
 }
