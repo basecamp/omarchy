3edef15907b26e96b56191ab64ca094380dbce90
David Heinemeier Hansson <david@hey.com>
2026-05-19 11:20:29 +0200

    Drop the systemd running on advice from outfoxxed

--- install/first-run/omarchy-shell.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p ~/.config/systemd/user
-cp "$OMARCHY_PATH/config/systemd/user/omarchy-shell.service" ~/.config/systemd/user/omarchy-shell.service
-systemctl --user daemon-reload
-systemctl --user enable --now omarchy-shell.service
