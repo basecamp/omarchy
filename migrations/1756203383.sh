a157b98d5b1d26b7c46245cc3aaa28a3bd845010
David Heinemeier Hansson <david@hey.com>
2026-05-19 18:51:05 +0200

    No longer need wiremix

--- migrations/1756203383.sh
+++ migrations/1756203383.sh
@@ -1,5 +1,4 @@
-echo "Remove old About, Activity, Audio Settings apps that are in Omarchy Menu or hotkey"
+echo "Remove old About and Activity apps that are in Omarchy Menu or hotkey"
 
 rm -f ~/.local/share/applications/About.desktop
 rm -f ~/.local/share/applications/Activity.desktop
-rm -f ~/.local/share/applications/wiremix.desktop
