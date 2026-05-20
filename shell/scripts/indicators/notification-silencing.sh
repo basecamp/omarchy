f6c6a6d32359ed819aadf7938c12ec52a8de341a
David Heinemeier Hansson <david@hey.com>
2026-05-20 14:34:15 +0200

    Simplify

--- shell/scripts/indicators/notification-silencing.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-state=$(omarchy-shell notifications isDnd 2>/dev/null || echo off)
-if [[ $state == "on" ]]; then
-  echo '{"text": "󰂛", "tooltip": "Notifications silenced", "class": "active"}'
-else
-  echo '{"text": ""}'
-fi
