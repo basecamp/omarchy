b081963455d90689f76475dc030bef690d7d2c7f
David Heinemeier Hansson <david@hey.com>
2026-05-21 12:43:03 +0200

    Use launcher hides list

--- migrations/1771651931.sh
+++
@@ -1,4 +0,0 @@
-echo "Hide limine-snapper-restore from app launcher"
-
-mkdir -p ~/.local/share/applications
-cp "$OMARCHY_PATH/applications/hidden/limine-snapper-restore.desktop" ~/.local/share/applications/
