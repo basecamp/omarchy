13eb0ac07772cf47381e5d276cffee53d10e514f
David Heinemeier Hansson <david@hey.com>
2026-05-22 09:39:26 +0200

    Beef up audio recovery when something goes wrong

--- migrations/1768916735.sh
+++ migrations/1768916735.sh
@@ -4,5 +4,5 @@ source "$OMARCHY_PATH/install/config/hardware/asus/fix-mic.sh"
 source "$OMARCHY_PATH/install/config/hardware/asus/fix-audio-mixer.sh"
 
 if omarchy-hw-asus-rog; then
-  omarchy-restart-pipewire
+  omarchy-restart-audio
 fi
