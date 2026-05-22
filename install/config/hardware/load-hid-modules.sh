919a76f16bfdef672dd83abbadcb679e09b19999
davi.oliveira <davi.oliveira@ernestoborges.com.br>
2026-05-22 16:29:29 -0400

    fix: add early-hid-modules.conf in mkinitcpio.conf.d/ for USB/wireless input devices at boot

---
+++ install/config/hardware/load-hid-modules.sh
@@ -0,0 +1,4 @@
+# Enable early module loading for USB/wireless HID devices at the boot screen
+if [[ ! -f /etc/mkinitcpio.conf.d/early-hid-modules.conf ]]; then
+  echo "MODULES=(xhci_pci usbhid hid_generic hid_apple)" | sudo tee /etc/mkinitcpio.conf.d/early-hid-modules.conf
+fi
