b29747a54b1abb0c2349e51f49d165c444eb38e3
Ryan Hughes <ryan@heyoodle.com>
2026-05-23 14:05:15 -0400

    installer: reduce offline finalizer work

--- install/login/all.sh
+++ install/login/all.sh
@@ -1,4 +1,6 @@
 run_logged $OMARCHY_INSTALL/login/default-keyring.sh
 run_logged $OMARCHY_INSTALL/login/sddm.sh
-run_logged $OMARCHY_INSTALL/login/hibernation.sh
+if install_mode_is online; then
+  run_logged $OMARCHY_INSTALL/login/hibernation.sh
+fi
 run_logged $OMARCHY_INSTALL/login/limine-snapper.sh
