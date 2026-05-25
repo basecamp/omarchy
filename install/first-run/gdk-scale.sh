2d86736fb0de6b441ed18768b221932f39edc29a
David Heinemeier Hansson <david@hey.com>
2026-05-25 22:41:01 +0200

    Update for the new monitors.lua

--- install/first-run/gdk-scale.sh
+++ install/first-run/gdk-scale.sh
@@ -1,3 +1,4 @@
 # Set the default GDK_SCALE from what the monitor is currently reporting
 
-sed -i -E "s|^([[:space:]]*env[[:space:]]*=[[:space:]]*GDK_SCALE,).*|\\1$(omarchy-hyprland-monitor-scaling)|" ~/.config/hypr/monitors.conf
+scale=$(omarchy-hyprland-monitor-scaling)
+sed -i -E "s|^local omarchy_gdk_scale = .*|local omarchy_gdk_scale = ${scale}|" "$HOME/.config/hypr/monitors.lua"
