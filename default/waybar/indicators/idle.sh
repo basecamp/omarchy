ab6d92a9c9896fa81a61469d9d447c8db1f5581a
Silviu Marchis <silviump@gmail.com>
2026-06-01 13:03:47 +0800

    feat: add lockscreen toggle to support screensaver-only mode

--- default/waybar/indicators/idle.sh
+++ default/waybar/indicators/idle.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-if pgrep -x hypridle >/dev/null; then
-  echo '{"text": ""}'
+if ! pgrep -x hypridle >/dev/null; then
+  echo '{"text": "󱫖", "tooltip": "Idle lock system disabled", "class": "active"}'
+elif omarchy-toggle-enabled lockscreen-off; then
+  echo '{"text": "󱅟", "tooltip": "Lockscreen disabled (Screensaver only)", "class": "active"}'
 else
-  echo '{"text": "󱫖", "tooltip": "Idle lock disabled", "class": "active"}'
+  echo '{"text": ""}'
 fi
