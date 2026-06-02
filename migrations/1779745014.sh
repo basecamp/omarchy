838bd5485d75f50c6a6f06a83f4d7cf50246855a
Abishek Neupane <717abishekneupane@gmail.com>
2026-06-02 13:42:43 +0545

    just remove with rm

--- migrations/1779745014.sh
+++ migrations/1779745014.sh
@@ -1,8 +1,5 @@
 echo "Remove old Brave Origin Beta flags file and symlink Brave Origin flags to brave-flags.conf"
 
 mkdir -p ~/.config
-rm -f ~/.config/brave-origin-beta-flags.conf
-
-if [[ ! -e ~/.config/brave-origin-flags.conf ]]; then
-  ln -s ~/.config/brave-flags.conf ~/.config/brave-origin-flags.conf
-fi
+rm -f ~/.config/brave-origin-beta-flags.conf ~/.config/brave-origin-flags.conf
+ln -s ~/.config/brave-flags.conf ~/.config/brave-origin-flags.conf
