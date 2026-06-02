37731366308e66e92891ac50dc2c61577bf8eed8
Abishek Neupane <717abishekneupane@gmail.com>
2026-06-02 13:48:15 +0545

    checks before cleaning

--- migrations/1779745014.sh
+++ migrations/1779745014.sh
@@ -1,5 +1,8 @@
 echo "Remove old Brave Origin Beta flags file and symlink Brave Origin flags to brave-flags.conf"
 
 mkdir -p ~/.config
-rm -f ~/.config/brave-origin-beta-flags.conf ~/.config/brave-origin-flags.conf
-ln -s ~/.config/brave-flags.conf ~/.config/brave-origin-flags.conf
+rm -f ~/.config/brave-origin-beta-flags.conf
+
+if [[ -e ~/.config/brave-flags.conf ]]; then
+  ln -sf ~/.config/brave-flags.conf ~/.config/brave-origin-flags.conf
+fi
