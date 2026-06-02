2562acc9d214e9c6f2ff467ad9c120bf3bffea0f
Abishek Neupane <717abishekneupane@gmail.com>
2026-06-02 13:39:58 +0545

    fixed symlink with relative target

--- migrations/1779745014.sh
+++ migrations/1779745014.sh
@@ -1,7 +1,8 @@
 echo "Remove old Brave Origin Beta flags file and symlink Brave Origin flags to brave-flags.conf"
 
+mkdir -p ~/.config
 rm -f ~/.config/brave-origin-beta-flags.conf
 
 if [[ ! -e ~/.config/brave-origin-flags.conf ]]; then
-  ln -s brave-flags.conf ~/.config/brave-origin-flags.conf
+  ln -s ~/.config/brave-flags.conf ~/.config/brave-origin-flags.conf
 fi
