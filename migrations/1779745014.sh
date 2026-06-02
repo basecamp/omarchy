71afc4f832ce98ec4798692eadb0f8f1f7514859
Abishek Neupane <717abishekneupane@gmail.com>
2026-06-02 13:32:06 +0545

    remove brave origin beta flags

--- migrations/1779745014.sh
+++ migrations/1779745014.sh
@@ -1,9 +1,3 @@
 echo "Remove old Brave Origin Beta flags file (renamed to brave-origin)"
 
-if [[ -L ~/.config/brave-origin-beta-flags.conf ]]; then
-  rm -f ~/.config/brave-origin-beta-flags.conf
-fi
-
-if [[ -e ~/.config/brave-origin-beta-flags.conf ]]; then
-  rm -f ~/.config/brave-origin-beta-flags.conf
-fi
+rm -f ~/.config/brave-origin-beta-flags.conf
