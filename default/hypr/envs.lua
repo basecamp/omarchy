278a85f3b6bdea34c7f8c1b9b0b6e5658a27a45d
Carmine Paolino <carmine@paolino.me>
2026-06-06 11:55:34 +0200

    Stop forcing Qt style override

--- default/hypr/envs.lua
+++ default/hypr/envs.lua
@@ -18,7 +18,6 @@ hl.env("HYPRCURSOR_SIZE", "24")
 -- Force all apps to use Wayland.
 hl.env("GDK_BACKEND", "wayland,x11,*")
 hl.env("QT_QPA_PLATFORM", "wayland;xcb")
-hl.env("QT_STYLE_OVERRIDE", "kvantum")
 hl.env("MOZ_ENABLE_WAYLAND", "1")
 hl.env("ELECTRON_OZONE_PLATFORM_HINT", "wayland")
 hl.env("OZONE_PLATFORM", "wayland")
