c0f9e9cb41bf702fb0ef99a6956cae68db262bcf
Axel Fontaine <axel@axelfontaine.com>
2026-07-21 16:42:21 +0200

    Docker: Ensure sudo works in cross-platform image builds

--- install/config/docker.sh
+++ install/config/docker.sh
@@ -1,1 +1,8 @@
 usermod -aG docker "$OMARCHY_INSTALL_USER"
+
+# Ensure sudo works in cross-platform Docker image builds
+for src in /usr/lib/binfmt.d/qemu-*-static.conf; do
+  dst="/etc/binfmt.d/$(basename "$src")"
+  install -Dm644 "$src" "$dst"
+  sed -i 's/:FP$/:OCF/' "$dst"
+done
