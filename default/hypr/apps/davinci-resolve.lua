ec9da050eef10a0d9f1dc5a8dbab41f78e74a1fa
Akshar Patel <aksharcommit@gmail.com>
2026-08-14 23:47:31 -0400

    Stop Resolve dialogs from recapturing pointer focus

--- default/hypr/apps/davinci-resolve.lua
+++ default/hypr/apps/davinci-resolve.lua
@@ -3,9 +3,10 @@
 o.window(".*[Rr]esolve.*", {
   float = true,
   stay_focused = true,
+  no_follow_mouse = true,
   tag = "-default-opacity",
   opacity = "1 1",
 })
 
 o.window({ class = ".*[Rr]esolve.*", title = "^DaVinci Resolve( Studio)? - .+$" }, { fullscreen = true })
-o.window({ class = ".*[Rr]esolve.*", title = "^(DaVinci Resolve( Studio)? - .+|Project Manager)$" }, { stay_focused = false })
+o.window({ class = ".*[Rr]esolve.*", title = "^(DaVinci Resolve( Studio)? - .+|Project Manager|Preferences|Find Directory)$" }, { stay_focused = false })
