65131c9b8d98b61bf69f641b41ace50800f7a5a0
Shawn Yeager <shawn@shawnyeager.com>
2026-08-15 18:31:23 -0500

    Keep Thunderbolt USB-C xHCI awake on Intel MacBooks

---
+++ migrations/1786836656.sh
@@ -0,0 +1,4 @@
+echo "Keep Thunderbolt USB-C xHCI awake on Intel MacBooks"
+
+# shellcheck source=install/config/hardware/apple/fix-tb-usb-wakeup.sh
+source "$OMARCHY_PATH/install/config/hardware/apple/fix-tb-usb-wakeup.sh"
