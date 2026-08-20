0ddd0096f8ac8dc1d282d1ad6ec833ea43841242
Jacob Roberts <jroberts.g1ql8@simplelogin.com>
2026-08-20 10:55:46 -0600

    Have bitwarden floating window config work on all chromium based browsers

--- default/hypr/apps/bitwarden.lua
+++ default/hypr/apps/bitwarden.lua
@@ -1,6 +1,6 @@
 o.window("^(Bitwarden)$", { no_screen_share = true, tag = "+floating-window" })
 
-o.window("chrome-nngceckbapebfimnlniiiahkandclblb-Default", {
+o.window("((google-)?[cC]hrom(e|ium)|[bB]rave-browser|[mM]icrosoft-edge|Vivaldi-stable|helium)-nngceckbapebfimnlniiiahkandclblb-Default", {
   no_screen_share = true,
   tag = "+floating-window",
 })
